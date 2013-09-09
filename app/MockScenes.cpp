2acab58008c1306f3d30b64b1346fc2b10578e4f
Pawe? Burgchardt <22456155+broadcastmonkey@users.noreply.github.com>
2013-09-10 01:21:12 +0200

    Chyba ?le si? liczy alpha ... (?) + troch? ma?o FPS?w mi wychodzi - extrude ?

--- app/MockScenes.cpp
+++ app/MockScenes.cpp
@@ -118,7 +118,7 @@ bv::BasicNode* CreateRect(bv::BasicNode* parent)
 	return rectNode;
 }
 
-void createMyRing(bv::BasicNode* parent,float offsetX,float offsetY,float startAngle,float endAngle)
+void createMyRing(bv::BasicNode* parent,float offsetZ,float offsetX,float offsetY,float startAngle,float endAngle)
 {
 	bv::BasicNode* mockTree = new bv::BasicNode();
 
@@ -130,7 +130,7 @@ void createMyRing(bv::BasicNode* parent,float offsetX,float offsetY,float startA
 
     float rs = 10.f;
 
-    color.addKey(2, glm::vec4(0,0,0,1));
+    color.addKey(2, glm::vec4(0.1,0,0,1));
     color.addKey(5, glm::vec4(1,0,0,1));
  
     color.setWrapMethod(bv::WrapMethod::clamp, bv::WrapMethod::pingPong);
@@ -143,8 +143,8 @@ void createMyRing(bv::BasicNode* parent,float offsetX,float offsetY,float startA
 
     si.setWrapMethod(bv::WrapMethod::repeat, bv::WrapMethod::pingPong);
     float s = 5.0f;
-    si.addKey(0.f, 0.5f);si.addKey(0.4f, 0.5f);si.addKey(0.5f, s * 0.6f);si.addKey(0.65f, 0.4f);si.addKey(0.8f, s * 0.9f);si.addKey(0.95f, 0.35f);si.addKey(1.05f, 0.5f);
     si.addKey(0.f, 0.1f);
+    si.addKey(0.2f, 1.2f);
     //si.addKey(2.4f, 2.5f);//si.addKey(0.5f, s * 0.6f);si.addKey(0.65f, 0.4f);si.addKey(0.8f, s * 0.9f);si.addKey(0.95f, 0.35f);si.addKey(1.05f, 0.5f);
 
 	mockTree->setGeometryShaderPlugin(new bv::ExtrudePlugin(si));
@@ -182,7 +182,7 @@ void createMyRing(bv::BasicNode* parent,float offsetX,float offsetY,float startA
 	transY.addKey(0.f,offsetY);
 	
 	bv::FloatInterpolator transZ;
-	transZ.addKey(0.f,0.1f);
+	transZ.addKey(0.f,offsetZ);
 
 	trans->addTranslation(transX,transY,transZ);
 
@@ -201,17 +201,17 @@ void createMyRing(bv::BasicNode* parent,float offsetX,float offsetY,float startA
         parent->addChild(mockTree);
     }
 }
-void NightRect(bv::BasicNode* parent, int number)
+void NightRect(bv::BasicNode* parent, int number,float offsetX,float offsetY)
 {
-	float RectWidth=0.1;
+	float RectWidth=0.04;
 	bv::BasicNode* rectNode = new bv::BasicNode();
 	bv::Vec4Interpolator color;
 	float atom=0.1;
-	color.addKey(number*atom, glm::vec4(0,0,0,1));
-	color.addKey(number*atom+2, glm::vec4(0,0,0,1));
+	color.addKey(number*atom, glm::vec4(0,0,0,0));
+	color.addKey(number*atom+2, glm::vec4(0,0,0,0));
 	color.addKey(number*atom+3, glm::vec4(1,0,0,1));
-	color.addKey(number*atom+4, glm::vec4(0,0,0,1));
-	color.addKey(number*atom+6, glm::vec4(0,0,0,1));
+	color.addKey(number*atom+4, glm::vec4(0,0,0,0));
+	color.addKey(number*atom+6, glm::vec4(0,0,0,0));
 	color.setWrapMethod(bv::WrapMethod::clamp, bv::WrapMethod::repeat);
 	bv::SolidColorPlugin* solidRect = new bv::SolidColorPlugin(color);
 	rectNode->setPixelShaderPlugin(solidRect);
@@ -221,11 +221,11 @@ void NightRect(bv::BasicNode* parent, int number)
     si.setWrapMethod(bv::WrapMethod::repeat, bv::WrapMethod::pingPong);
     float s = 5.0f;
     si.addKey(0.f, 0.1f);
-    si.addKey(1.f, 0.3f);
+    //si.addKey(1.f, 0.3f);
     //si.addKey(2.4f, 2.5f);//si.addKey(0.5f, s * 0.6f);si.addKey(0.65f, 0.4f);si.addKey(0.8f, s * 0.9f);si.addKey(0.95f, 0.35f);si.addKey(1.05f, 0.5f);
 	rectNode->setGeometryShaderPlugin(new bv::ExtrudePlugin(si));
 	
-	rectNode->addGeometryPlugin(new bv::PluginGeometryRect(RectWidth,0.2f));
+	rectNode->addGeometryPlugin(new bv::PluginGeometryRect(RectWidth,0.1f));
 
 	// b edzie sie rozszerzal
 
@@ -241,7 +241,7 @@ void NightRect(bv::BasicNode* parent, int number)
     bv::PluginTransformSimple* transPlugin = bv::ModelFactory::CreatePluginTransformSimple(*trans);
 	
 	rectNode->addTransformPlugin(transPlugin);
-	rectNode->addTransformPlugin(FuncTranslate(-4+number*RectWidth*1.7,0,0.2));
+	rectNode->addTransformPlugin(FuncTranslate(offsetX,offsetY,-4+number*RectWidth*6));
 	if(parent != nullptr)
     {
         parent->addChild(rectNode);
@@ -254,7 +254,12 @@ void NightRider(bv::BasicNode* parent)
 
 	for(int i=0;i<50;i++)
 	{
-		NightRect(parent,i);
+		NightRect(parent,i,-2,0);
+		NightRect(parent,i,2,0);
+		NightRect(parent,i,0,-2);
+		NightRect(parent,i,0,2);
+		
+
 	}
 	/*if(parent != nullptr)
     {
@@ -262,22 +267,33 @@ void NightRider(bv::BasicNode* parent)
     }*/
 }
 
-bv::BasicNode* SceneExamples::CreateSceneMock3(bv::BasicNode* parent)
+void CreateRings(bv::BasicNode* parent,float z)
 {
-	bv::BasicNode* rect=CreateRect(parent);
-
 	float Angle1=3.1415f;
 	float Angle2=3.1415f/2.0;
 	float Angle3=0;
 	float Angle4=-3.1415f/2;
 	float offset=-3.1415f/2;
+	createMyRing(parent,z,-1,0.5,Angle1,Angle1+offset);	
+	createMyRing(parent,z,1,0.5,Angle2,Angle2+offset);
+	
+	createMyRing(parent,z,1,-0.5,Angle3, Angle3+offset);	
+	createMyRing(parent,z,-1,-0.5,Angle4,Angle4+offset);
+	
+}
 
-	createMyRing(rect,-1,0.5,Angle1,Angle1+offset);	
-	createMyRing(rect,1,0.5,Angle2,Angle2+offset);
+bv::BasicNode* SceneExamples::CreateSceneMock3(bv::BasicNode* parent)
+{
+	bv::BasicNode* rect=CreateRect(parent);
+
 	
-	createMyRing(rect,1,-0.5,Angle3, Angle3+offset);	
-	createMyRing(rect,-1,-0.5,Angle4,Angle4+offset);
-	
+
+	CreateRings(rect,0);
+	CreateRings(rect,-3);
+	CreateRings(rect,-6);
+	CreateRings(rect,-9);
+	CreateRings(rect,-12);
+	CreateRings(rect,-15);
 
 	AnimLines(rect);
 	NightRider(rect);
