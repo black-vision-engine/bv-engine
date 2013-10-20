40fcb1468f1936322eb631ea83b7c7befb5201d1
viggith <viggith@localhost>
2013-10-20 21:44:51 +0200

    Generic geometry plugin - just a placeholder

--- src/Engine/Models/Plugins/GeometryPlugin.h
+++ src/Engine/Models/Plugins/GeometryPlugin.h
@@ -1,5 +1,33 @@
 #pragma once
 
+#include "Engine/Models/Plugins/Plugin.h"
+#include "Engine/Models/Plugins/PluginParameters.h"
+#include "Mathematics/Transform/MatTransform.h"
+
 namespace bv { namespace model { 
+
+// ***************************** DESCRIPTOR **********************************
+class GeometryPluginPD : public BaseParametersDescriptor
+{
+public:
+    static const std::string            pluginName;
+
+    explicit GeometryPluginPD();
+};
+
+
+// ***************************** PLUGIN ********************************** 
+class GeometryPlugin : public BasePlugin< IPlugin, GeometryPluginPD >
+{
+public:
+
+    explicit            GeometryPlugin  ();
+                        ~GeometryPlugin ();
+
+    void                Update          ( float t ) override;
+    void                Print           ( std::ostream & out, int tabs = 0 ) const override;
+
+};
+
 }
 }
