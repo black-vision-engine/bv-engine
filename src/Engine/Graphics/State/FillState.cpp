dbe95b50bae0600fe4a9af809be20e86dd1d5d55
viggith <viggith@localhost>
2013-10-28 19:09:21 +0100

    Interesting ding

--- src/Engine/Graphics/State/FillState.cpp
+++ src/Engine/Graphics/State/FillState.cpp
@@ -9,4 +9,34 @@ FillState::FillState  ()
 {
 }
 
+
+class A
+{
+public:
+
+    int b() { return 0; };
+
+private:
+
+    int b( int a ) { return a + b(); }
+
+public:
+
+    int (A::*get_pptr())(int)
+    {
+        return &A::b;
+    }
+};
+
+int w( A * a )
+{
+    //&A::b;
+    //int k = (char*) (&A::b);
+    int (A::*memberPtr0)(int) = a->get_pptr();
+    int (A::*memberPtr1)() = &A::b;
+
+    return (a->*memberPtr0)( 10 );
+    return (a->*memberPtr1)();
+};
+
 }
