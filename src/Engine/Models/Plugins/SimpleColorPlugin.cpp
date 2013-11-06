af614294cb5e47d8f4777d2bf9205057f7ca473f
Radek Zagorowicz <Radek Zagorowicz@localhost>
2013-11-06 22:56:27 +0100

    -jeszcze sie nie kompiluje.

---
+++ src/Engine/Models/Plugins/SimpleColorPlugin.cpp
@@ -0,0 +1,94 @@
+
+#include "SimpleColorPlugin.h"
+#include "Engine/Models/Plugins/Channels/ShaderChannel.h"
+#include "Engine/Models/Plugins/Parameter.h"
+
+namespace bv { namespace model {
+
+class SimpleColorPixelShaderChannelPD : public BaseParametersDescriptor
+{
+public:
+    static const std::string            pluginName;// FIXME: To jest tu niepotrzebne
+    static const std::string            colorParamName;
+
+    explicit SimpleColorPixelShaderChannelPD()
+        : BaseParametersDescriptor( pluginName ) 
+    {
+    }
+};
+
+const std::string SimpleColorPixelShaderChannelPD::pluginName       = "PluginName";
+const std::string SimpleColorPixelShaderChannelPD::colorParamName   = "color";
+
+class SimpleColorPixelShaderChannel : public ShaderChannel< model::IPixelShaderChannel, SimpleColorPixelShaderChannelPD >
+{
+    model::ValueVec4*   m_colorVal;
+    Vec4Interpolator    m_color;
+
+public:
+
+    explicit                        SimpleColorPixelShaderChannel( const Vec4Interpolator& color )
+        : ShaderChannel( "" )// FIXME:
+        , m_color( color )
+    {
+        m_colorVal = new model::ValueVec4( ParamDesc::colorParamName );
+        RegisterValue( m_colorVal );
+    }
+
+    virtual void                    Update( float t )
+    {
+        m_colorVal->SetValue( m_color.evaluate( t ) );
+        ShaderChannel::Update( t );
+    }
+
+};
+
+SimpleColorPlugin::SimpleColorPlugin          ( const IPlugin * prev, const Vec4Interpolator& color )
+    : m_prev( prev )
+{
+    auto pixelShaderChannel = new SimpleColorPixelShaderChannel( color );
+
+    m_pshaderChannel = pixelShaderChannel;
+}
+
+SimpleColorPlugin::~SimpleColorPlugin         ()
+{
+}
+
+const IGeometryChannel*         SimpleColorPlugin::GetGeometryChannel          () const
+{
+    return m_prev->GetGeometryChannel();
+}
+
+const ITransformChannel*        SimpleColorPlugin::GetTransformChannel         () const
+{
+    return m_prev->GetTransformChannel();
+}
+
+const IPixelShaderChannel*      SimpleColorPlugin::GetPixelShaderChannel       () const
+{
+    return m_pshaderChannel;
+}
+
+const IVertexShaderChannel*     SimpleColorPlugin::GetVertexShaderChannel      () const
+{
+    return m_prev->GetVertexShaderChannel();
+}
+
+const IGeometryShaderChannel*   SimpleColorPlugin::GetGeometryShaderChannel    () const
+{
+    return m_prev->GetGeometryShaderChannel();
+}
+
+void                            SimpleColorPlugin::Update                      ( float t )
+{
+    BasePlugin::Update( t );
+}
+
+void                            SimpleColorPlugin::Print                       ( std::ostream & out, int tabs ) const
+{
+    out << GetName() << std::endl;
+}
+
+} // model
+} // bv
---
+++ src/Engine/Models/Plugins/SimpleColorPlugin.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "Engine/Models/Plugins/Plugin.h"
+#include "Engine/Models/Plugins/PluginParameters.h"
+
+namespace bv { namespace model {
+
+
+// ***************************** DESCRIPTOR **********************************
+class SimpleColorPluginPD : public BaseParametersDescriptor
+{
+public:
+
+    static const std::string            pluginName;
+
+    explicit SimpleColorPluginPD();
+};
+
+
+// ***************************** PLUGIN ********************************** 
+class SimpleColorPlugin : public BasePlugin< IPlugin, SimpleColorPluginPD >
+{
+private:
+
+    const IPlugin *     m_prev;
+
+public:
+
+    explicit                                SimpleColorPlugin           ( const IPlugin * prev, const Vec4Interpolator& color );
+                                            ~SimpleColorPlugin          ();
+
+    virtual const IGeometryChannel*         GetGeometryChannel          () const;
+    virtual const ITransformChannel*        GetTransformChannel         () const;
+    virtual const IPixelShaderChannel*      GetPixelShaderChannel       () const;
+    virtual const IVertexShaderChannel*     GetVertexShaderChannel      () const;
+    virtual const IGeometryShaderChannel*   GetGeometryShaderChannel    () const;
+
+    void                                    Update                      ( float t ) override;
+    void                                    Print                       ( std::ostream & out, int tabs = 0 ) const override;
+
+};
+
+} // model
+} // bv
