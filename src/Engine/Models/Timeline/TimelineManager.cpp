957a446d19677f715bd173ac66e4229002622489
viggith <viggith@localhost>
2013-11-19 00:18:05 +0100

    Timeline manager registers timelines via name right now

--- src/Engine/Models/Timeline/TimelineManager.cpp
+++ src/Engine/Models/Timeline/TimelineManager.cpp
@@ -9,7 +9,7 @@ std::vector< const ITimeEvaluator * >   TimelineManager::m_timelines;
 
 // *********************************
 //
-void TimelineManager::RegisterTimeline( const ITimeEvaluator * timeline )
+void TimelineManager::RegisterTimeline( const std::string & name, const ITimeEvaluator * timeline )
 {
     //FIXME: do not let two identical timelines to be registered
     m_timelines.push_back( timeline );
--- src/Engine/Models/Timeline/TimelineManager.h
+++ src/Engine/Models/Timeline/TimelineManager.h
@@ -1,7 +1,8 @@
 #pragma once
 
 #include <vector>
-
+#include <hash_map>
+#include <string>
 
 namespace bv { namespace model {
 
@@ -12,10 +13,11 @@ class TimelineManager
 private:
 
     static std::vector< const ITimeEvaluator * >   m_timelines;
+    static std::hash_map< std::string, const ITimeEvaluator * > m_mappedTimelines;
 
 public:
 
-    static void RegisterTimeline( const ITimeEvaluator * timeline );
+    static void RegisterTimeline( const std::string & name, const ITimeEvaluator * timeline );
     static void Cleanup         ();
 
 };
