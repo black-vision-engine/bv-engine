a47abed3d682b7e0ece432e75e6f3d1f53270ad8
viggith <viggith@localhost>
2013-12-02 23:41:07 +0100

    post commit

--- src/Engine/Processes/ProcessThreaded.h
+++ src/Engine/Processes/ProcessThreaded.h
@@ -1,6 +1,25 @@
 #pragma once
 
+#include "System/Threading/Thread.h"
+#include "Engine/Processes/Process.h"
+
 
 namespace bv
 {
+
+class ProcessThreaded : public Process, public Thread
+{
+private:
+
+public:
+
+                    ProcessThreaded     ();
+    virtual         ~ProcessThreaded    ();
+
+protected:
+
+    virtual void    OnUpdate            ( unsigned long millis ) override;
+
+};
+
 } //bv
