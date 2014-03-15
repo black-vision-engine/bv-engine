8db9bbdc9287924673901a687ae7dfc11e633431
Radek Zagorowicz <Radek Zagorowicz@localhost>
2014-03-15 19:00:38 +0100

    -usprawnienia w TimerPlugin.

--- src/Engine/Models/Plugins/Simple/TimerPlugin.h
+++ src/Engine/Models/Plugins/Simple/TimerPlugin.h
@@ -24,14 +24,28 @@ public:
 
 struct TimeValue
 {
-    int     setSecond;
+    int     fracOfSecond;
     int     second;
     int     minute;
     int     hour;
 
-    explicit    TimeValue( double time );
+    explicit    TimeValue( double time, int accuracy = 2 );
 
     bool        operator!=(const TimeValue& other) const;
+};
+
+struct TimeInfo
+{
+    int hoursPHStart;
+    int hoursPlaceholderSize; // 0..
+    int minutesPHStart;
+    int minutesPlaceHolderSize; // 0..2
+    int secondsPHStart;
+    int secondsPlaceHolderSize; // 0..2
+    int fosPHStart;
+    int fracOfSecondsPlaceholderSize; // 0..
+
+    int GetSize() const;
 };
 
 // ***************************** PLUGIN ********************************** 
@@ -39,6 +53,7 @@ class TimerPlugin : public BasePlugin< IPlugin, TimerPluginUID >
 {
     ParamFloat                  m_timeParam;
     TimeValue                   m_currentTime;
+    const wchar_t               m_defaultSeparator;
 
     VertexAttributesChannelPtr  m_vertexAttributeChannel;
 
@@ -48,6 +63,7 @@ class TimerPlugin : public BasePlugin< IPlugin, TimerPluginUID >
     const TextAtlas*            m_currentAtlas;
 
     std::wstring                m_timePatern;
+    TimeInfo                    m_timePaternInfo;
 
     explicit                    TimerPlugin     ( const ParamFloat& timeParam, unsigned int fontSize );
 
@@ -55,6 +71,7 @@ class TimerPlugin : public BasePlugin< IPlugin, TimerPluginUID >
 
     void                        SetValue        ( unsigned int connComp, wchar_t wch );
     const GlyphCoords&          GetGlyphCoords  ( wchar_t wch ) const;
+    void                        Refresh         ();
 
 public:
 
@@ -63,6 +80,10 @@ public:
     void                                        SetTimePatern               ( const std::wstring& patern );
     void                                        SetTime                     ( const std::wstring& time );
     void                                        SetTime                     ( double time );
+    void                                        SetHOSecond                 ( int hoSec );
+    void                                        SetSecond                   ( int sec );
+    void                                        SetMinute                   ( int min );
+    void                                        SetHour                     ( int h );
 
     virtual const IVertexAttributesChannel *    GetVertexAttributesChannel  () const override;
     virtual Textures                            GetTextures                 () const override;
