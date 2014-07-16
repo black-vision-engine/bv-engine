0afd9149523c9ab8537581a1c2415bef1b59a6d0
viggith <viggith@fewsquares.com>
2014-07-16 02:12:17 +0200

    demoscena :D

--- app/NewModelTestScenes.cpp
+++ app/NewModelTestScenes.cpp
@@ -465,8 +465,8 @@ model::BasicNodePtr     TestScenesFactory::NewModelTestScene     ( const model::
 
     //return rut;
     //return SimpleNodesFactory::CreateGreenRectNodeNoAssert( timelineManager, timeEvaluator, true );
-    //return SimpleNodesFactory::CreateTexturedRectNode( timelineManager, timeEvaluator, false );
-    return SimpleNodesFactory::CreateTextureAnimationRectNode( timelineManager, timeEvaluator, false );
+    return SimpleNodesFactory::CreateTexturedRectNode( timelineManager, timeEvaluator, true );
+    //return SimpleNodesFactory::CreateTextureAnimationRectNode( timelineManager, timeEvaluator, false );
     //return SimpleNodesFactory::CreateTextNode( timelineManager, timeEvaluator, 0, false );
     //return SimpleNodesFactory::CreateTextWithShadowNode( timelineManager, timeEvaluator, 5, glm::vec3( 0.005f, -0.005f, -0.001f ) );
 }
