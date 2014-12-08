1b390ee535f91cc642806039f90d9da321fdc113
viggith <viggith@fewsquares.com>
2014-12-08 17:35:11 +0100

    yellow test

--- app/testai/AICommands.cpp
+++ app/testai/AICommands.cpp
@@ -316,10 +316,12 @@ bool        AICommandEnableOverridenAlpha::TriggerImpl          ( TimeType t )
     {
         auto state = m_node->GetOverrideState();
         auto alpha = state->GetAlphaParam();
-        SetParameter( alpha, t, 1.f );
+        SetParameter( alpha, 0.f, 0.7f );
+     
+        /*SetParameter( alpha, t, 1.f );
         SetParameter( alpha, t + 2.5f, 0.f );
         SetParameter( alpha, t + 5.5f, 0.f );
-        SetParameter( alpha, t + 8.f, 1.f );
+        SetParameter( alpha, t + 8.f, 1.f );*/
         m_node->EnableOverrideState();
 
         return true;
--- app/testai/TestAIManager.cpp
+++ app/testai/TestAIManager.cpp
@@ -261,28 +261,35 @@ TestAI *        TestAIManager::PreparePreset3   ( model::IModelNodePtr node ) co
     auto timeline = model::DefaultTimelinePtr( new model::DefaultTimeline( "timeline preset 0", TimeType( 30.0 ), TimelineWrapMethod::TWM_CLAMP, TimelineWrapMethod::TWM_CLAMP ) );
 
     TestAI * ai = new TestAI( timeline, node );
+    
+    auto c0 = new AICommandEnableOverridenAlpha( node, ".", 2.f );
+    auto c1 = new AICommandEnableOverridenAlpha( node, "./node0/node02", 5.f );
+    auto c2 = new AICommandDisableAlpha( node, ".", 20.f );
+    auto c3 = new AICommandDisableAlpha( node, "./node0/node02", 22.f );
 
+/*
     auto c0 = new AICommandEnableOverridenAlpha( node, "./node0/node02", 0.f );
     auto c1 = new AICommandEnableOverridenAlpha( node, "./node0", 1.f );
     auto c2 = new AICommandEnableOverridenAlpha( node, ".", 2.f );
     auto c3 = new AICommandDisableAlpha( node, "./node0/node02", 10.f );
     auto c4 = new AICommandDisableAlpha( node, "./node0", 11.f );
     auto c5 = new AICommandDisableAlpha( node, ".", 12.f );
-    auto c6 = new AICommandEnableOverridenAlpha( node, "./node0/node02", 13.f );
-    auto c7 = new AICommandDisableAlpha( node, "./node0/node02", 23.f );
-    auto c8 = new AICommandEnableOverridenAlpha( node, ".", 26.f );
-    auto c9 = new AICommandDisableAlpha( node, ".", 36.f );
+*/
+//    auto c6 = new AICommandEnableOverridenAlpha( node, "./node0/node02", 13.f );
+//    auto c7 = new AICommandDisableAlpha( node, "./node0/node02", 23.f );
+//    auto c8 = new AICommandEnableOverridenAlpha( node, ".", 26.f );
+//    auto c9 = new AICommandDisableAlpha( node, ".", 36.f );
 
     ai->AddCommand( c0 );
     ai->AddCommand( c1 );
     ai->AddCommand( c2 );
     ai->AddCommand( c3 );
-    ai->AddCommand( c4 );
-    ai->AddCommand( c5 );
-    ai->AddCommand( c6 );
-    ai->AddCommand( c7 );
-    ai->AddCommand( c8 );
-    ai->AddCommand( c9 );
+//    ai->AddCommand( c4 );
+//    ai->AddCommand( c5 );
+//    ai->AddCommand( c6 );
+//    ai->AddCommand( c7 );
+//    ai->AddCommand( c8 );
+//    ai->AddCommand( c9 );
 
     return ai;
 }
