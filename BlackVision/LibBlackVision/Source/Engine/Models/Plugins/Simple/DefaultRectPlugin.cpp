73508d89de5550ba600a5781487a7caaf82c49d7
creed <creed@localhost>
2015-03-06 16:13:19 +0100

    Platonic CreateDefaultModel (only in comments by now)

--- BlackVision/LibBlackVision/Source/Engine/Models/Plugins/Simple/DefaultRectPlugin.cpp
+++ BlackVision/LibBlackVision/Source/Engine/Models/Plugins/Simple/DefaultRectPlugin.cpp
@@ -48,6 +48,18 @@ IPluginPtr                      DefaultRectPluginDesc::CreatePlugin         ( co
 //
 DefaultPluginParamValModelPtr   DefaultRectPluginDesc::CreateDefaultModel   ( ITimeEvaluatorPtr timeEvaluator ) const
 {
+   // START_MODEL( timeEvaluator )
+   //     ADD_VAC_PARAM( "width", 1.f );
+   //     ADD_VAC_PARAM( "height", 1.f );
+   //     ADD_VAC_PARAM_VAL( "param0", glm::vec2( .4f, 0.f ) );
+   //     START_COMPOSITE_PARAM( vec4(0,0,0,0) )
+   //         ADD_VAC_PARAM( "r", 1.f );
+   //         ADD_VAC_PARAM( "g", 1.f );
+   //         ADD_VAC_PARAM( "b", 1.f );
+   //         ADD_VAC_PARAM( "a", 1.f );
+   //     END_COMPOSITE_PARAM()
+   //END_MODEL()
+
     DefaultPluginParamValModelPtr   model       = std::make_shared< DefaultPluginParamValModel >();
     DefaultParamValModelPtr         vacModel    = std::make_shared< DefaultParamValModel >();
 
