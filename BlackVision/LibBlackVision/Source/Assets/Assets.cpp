6428606ca35153b7e28793401c5d674b46693f79
Radek Zagorowicz <radoslaw.zagorowicz@imapp.pl>
2015-03-24 11:25:26 +0100

    Adding high-level api for loading assets.

---
+++ BlackVision/LibBlackVision/Source/Assets/Assets.cpp
@@ -0,0 +1,49 @@
+#include "Assets.h"
+
+namespace bv
+{
+
+// ***********************
+//
+TextureAssetConstPtr	LoadTextureAsset( const std::string & imageFilePath, bool isCacheable )
+{
+	return std::static_pointer_cast< const TextureAsset >( AssetManager::GetInstance().LoadAsset( TextureAssetDesc::Create( imageFilePath, isCacheable ) ) );
+}
+
+// ***********************
+//
+TextureAssetConstPtr	LoadTextureAsset( const std::string & imageFilePath, MipMapFilterType mmFilter, bool isCacheable )
+{
+	return std::static_pointer_cast< const TextureAsset >( AssetManager::GetInstance().LoadAsset( TextureAssetDesc::Create( imageFilePath, mmFilter, isCacheable ) ) );
+}
+
+// ***********************
+//
+TextureAssetConstPtr	LoadTextureAsset( const std::string & imageFilePath, const StringVector & mipMapsPaths, bool isCacheable )
+{
+	return std::static_pointer_cast< const TextureAsset >( AssetManager::GetInstance().LoadAsset( TextureAssetDesc::Create( imageFilePath, mipMapsPaths, isCacheable ) ) );
+}
+
+// ***********************
+//
+FontAssetConstPtr	LoadFontAsset	(	const std::string & fontFileName,
+										UInt32 fontSize,
+										UInt32 blurSize,
+										UInt32 outlineSize,
+										bool generateMipmaps )
+{
+	return std::static_pointer_cast< const FontAsset >( AssetManager::GetInstance().LoadAsset( FontAssetDesc::Create( fontFileName,
+										fontSize,
+										blurSize,
+										outlineSize,
+										generateMipmaps ) ) );
+}
+
+// ***********************
+//
+AnimationAssetConstPtr LoadAnimationAsset( const std::string & path, const std::string & filter )
+{
+	return std::static_pointer_cast< const AnimationAsset >( AssetManager::GetInstance().LoadAsset( AnimationAssetDesc::CreateFromDirFrames( path, filter ) ) );
+}
+
+} // bv
