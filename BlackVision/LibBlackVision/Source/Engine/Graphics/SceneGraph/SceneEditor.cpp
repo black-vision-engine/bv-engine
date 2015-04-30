09bcf12262e70d268ab6281354872932584b6373
viggith <viggith@fewsquares.com>
2015-04-30 20:57:57 +0200

    SceneEditor API ready

--- BlackVision/LibBlackVision/Source/Engine/Graphics/SceneGraph/SceneEditor.cpp
+++ BlackVision/LibBlackVision/Source/Engine/Graphics/SceneGraph/SceneEditor.cpp
@@ -1,2 +1,102 @@
 #include "SceneEditor.h"
 
+#include "Engine/Graphics/SceneGraph/SceneNode.h"
+
+
+namespace bv {
+
+// *******************************
+//
+SceneEditor::SceneEditor					()
+{
+}
+
+// *******************************
+//
+void		SceneEditor::SetRootNode		( SceneNode * rootNode )
+{
+	// FIXME: implement
+	{ rootNode; }
+}
+
+// *******************************
+//
+bool		SceneEditor::DeleteRootNode     ( SceneNode * rootNode )
+{
+	// FIXME: implement
+	{ rootNode; }
+	return false;
+}
+
+// *******************************
+//
+void		SceneEditor::AddChildNode       ( SceneNode * parentNode, SceneNode * childNode )
+{
+	// FIXME: implement
+	{ parentNode; childNode; }
+}
+
+// *******************************
+//
+bool		SceneEditor::DeleteChildNode    ( SceneNode * parentNode, SceneNode * childNode )
+{
+	// FIXME: implement
+	{ parentNode; childNode; }
+
+	return false;
+}
+
+// *******************************
+//
+void		SceneEditor::AttachRootNode     ( SceneNode * rootNode )
+{
+	// FIXME: implement
+	{ rootNode; }
+}
+
+// *******************************
+//
+bool		SceneEditor::DetachRootNode     ( SceneNode * rootNode )
+{
+	// FIXME: implement
+	{ rootNode; }
+
+	return false;
+}
+
+// *******************************
+//
+bool		SceneEditor::AttachChildNode    ( SceneNode * parentNode )
+{
+	// FIXME: implement
+	{ parentNode; }
+
+	return false;
+}
+
+// *******************************
+//
+bool		SceneEditor::DetachChildNode    ( SceneNode * parentNode, SceneNode * childNode )
+{
+	// FIXME: implement
+	{ parentNode; childNode; }
+
+	return false;
+}
+
+// *******************************
+//
+SceneNode *	SceneEditor::GetDetachedNode	()
+{
+	// FIXME: implement
+	return nullptr;
+}
+
+// *******************************
+//
+void		SceneEditor::DeleteDetachedNode	()
+{
+	// FIXME: implement
+}
+
+} //bv
--- BlackVision/LibBlackVision/Source/Engine/Graphics/SceneGraph/SceneEditor.h
+++ BlackVision/LibBlackVision/Source/Engine/Graphics/SceneGraph/SceneEditor.h
@@ -1,4 +1,35 @@
 #pragma once
 
+
 namespace bv {
+
+class SceneNode;
+
+class SceneEditor
+{
+private:
+
+	SceneNode *		m_detachedNode;
+
+public:
+
+							SceneEditor			();
+
+    void                    SetRootNode         ( SceneNode * rootNode );
+    bool                    DeleteRootNode      ( SceneNode * rootNode );
+
+    void                    AddChildNode        ( SceneNode * parentNode, SceneNode * childNode );
+    bool                    DeleteChildNode     ( SceneNode * parentNode, SceneNode * childNode );
+
+    void                    AttachRootNode      ( SceneNode * rootNode );
+    bool                    DetachRootNode      ( SceneNode * rootNode );
+
+	bool                    AttachChildNode     ( SceneNode * parentNode );
+    bool                    DetachChildNode     ( SceneNode * parentNode, SceneNode * childNode );
+
+    SceneNode *				GetDetachedNode     ();
+    void                    DeleteDetachedNode  ();
+
+};
+
 } //bv
