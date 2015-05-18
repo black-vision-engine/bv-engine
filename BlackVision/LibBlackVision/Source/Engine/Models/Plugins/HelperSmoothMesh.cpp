701772da6faecfddd203f3b4510c906af354fbcc
WitekD <WitekD@localhost>
2015-05-18 13:26:59 +0200

    SmoothMesh - tesseletion of mesh written (not tested)

--- BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperSmoothMesh.cpp
+++ BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperSmoothMesh.cpp
@@ -26,7 +26,22 @@ void HelperSmoothMesh::smooth( IndexedGeometry& mesh, std::vector<unsigned short
 {
 	if( tesselation == 0 )
 		resultMesh = mesh;		// Normal copy assignment.
-	private_smooth( mesh, edges, tesselation, resultMesh );
+	privateSmooth( mesh, edges, tesselation, resultMesh );
+}
+
+/**This is main function, that should be called. It tmake given mesh smooth using number of tesselations in param.
+
+Function calls private smooth to protect user from hurt himself. If he called private_smooth function with
+tesselation = 0, function would move content of mesh to resultMesh, leaving mesh in bad shape.
+@param[in] mesh Begin mesh to smooth.
+@param[in] edges Edges which should remain sharp.
+@param[in] tesselation Number of tesselations, that will be aplied to mesh
+@return resultMesh Result of smooth.*/
+IndexedGeometry HelperSmoothMesh::smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation )
+{
+	IndexedGeometry new_mesh;
+	smooth( mesh, edges, tesselation, new_mesh );
+	return std::move( new_mesh );
 }
 
 /**This the real smooth function. Look at the description of smooth, if you want to know more.
@@ -35,7 +50,7 @@ void HelperSmoothMesh::smooth( IndexedGeometry& mesh, std::vector<unsigned short
 @param[in] edges Edges which should remain sharp.
 @param[in] tesselation Number of tesselations, that will be aplied to mesh
 @param[out] resultMesh Result of smooth.*/
-void HelperSmoothMesh::private_smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh )
+void HelperSmoothMesh::privateSmooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh )
 {
 	if( tesselation == 0 )	// End of recursion.
 	{
@@ -43,10 +58,10 @@ void HelperSmoothMesh::private_smooth( IndexedGeometry& mesh, std::vector<unsign
 		return;
 	}
 
-	IndexedGeometry new_mesh( mesh );
+	IndexedGeometry new_mesh;
 
 	tesselate( mesh, new_mesh );
-	move_verticies( mesh, edges, resultMesh );
+	moveVerticies( mesh, edges, resultMesh );
 
 	smooth( new_mesh, edges, tesselation - 1, resultMesh );
 	// Remember! You can't do anything with new_mesh after smooth call. It have been already moved to resultMesh.
@@ -55,22 +70,67 @@ void HelperSmoothMesh::private_smooth( IndexedGeometry& mesh, std::vector<unsign
 /**Tesselates given mesh one time.*/
 void HelperSmoothMesh::tesselate( IndexedGeometry& mesh, IndexedGeometry& resultMesh )
 {
-	resultMesh;
 	std::vector<unsigned short>& indicies = mesh.getIndicies();
 	std::vector<glm::vec3>& verticies = mesh.getVerticies();
-	std::vector<bool> verticies_bitset;
+	std::vector<unsigned short>& resultIndicies = mesh.getIndicies();
+	std::vector<glm::vec3>& resultVerticies = resultMesh.getVerticies();
 
+	std::vector<bool> usedVerticies;
 
-	verticies_bitset.resize( verticies.size() );
+	resultMesh.getIndicies().reserve( 4 * indicies.size() );
+	std::copy( verticies.begin(), verticies.end(), resultVerticies.begin() );		//Existing verticies have the same position as in init geometry.
+	resultMesh.getVerticies().reserve( 3 * verticies.size() / 2 );
+
+	usedVerticies.resize( verticies.size(), false );
 
 	for( unsigned int i = 0; i < indicies.size(); i += 3 )
 	{
+		unsigned short newIndicies[3];
 
+		for( unsigned int j = 0; j < 3; ++j )
+			if( usedVerticies[ indicies[i + j] ] && usedVerticies[ indicies[i + j + 1] ] )
+			{// This edge have been already devided.
+				// Looking for vertex containing this edge in resultIndicies. 
+				for( unsigned int k = 0; k < resultIndicies.size(); k += 12 )	// We have been adding 12 indicies at one time.
+					for( int v = 0; v < 3; v += 3 )		// Onlu 3 verticies are interesting.
+						if( resultIndicies[ k + v ] == indicies[i + j] )	// Indicies are in the same order
+							if( resultIndicies[ k + (v+3) % 9] == indicies[i + j + 1] )
+							{// We found it
+								newIndicies[j] = resultIndicies[ k + 9 + v / 3 ];	// Thats because we know, how we made this table.
+							}
+						else if( resultIndicies[ k + v ] == indicies[i + j + 1] )	// Indicies are in diffrent order.
+							if( resultIndicies[ k + (v+3) % 9] == indicies[i + j] )
+							{// We found it
+								newIndicies[j] = resultIndicies[ k + 9 + v / 3];	// Thats because we know, how we made this table.
+							}
+			}
+			else
+			{// We must devide edge.
+				glm::vec3 firstVertex = verticies[ indicies[i + j] ];
+				glm::vec3 secondVertex = verticies[ indicies[i + j + 1] ];
+				resultVerticies.push_back( ( firstVertex + secondVertex ) * glm::vec3( 0.5, 0.5, 0.5 ) );
+
+				newIndicies[j] = static_cast<unsigned short>( resultVerticies.size() - 1 );		// Position of new added vertex in vector is out new index;
+
+				usedVerticies[ indicies[i + j] ] = true;		// Set verticies as used.
+				usedVerticies[ indicies[i + j + 1] ] = true;	// Set verticies as used.
+			}
+
+		// Adding new traingles. Don't touch adding order. Function depeds on it, when vertex already existed.
+		int k = 2;
+		for( int j = 0; j < 3; ++j )	// Outer triangles.
+		{
+			resultIndicies.push_back( static_cast<unsigned short>( i + j ) );
+			resultIndicies.push_back( newIndicies[ k++ % 3 ] );
+			resultIndicies.push_back( newIndicies[ k % 3 ] );
+		}
+		for( int j = 0; j < 3; ++j )	// Middle triangle.
+			resultIndicies.push_back( newIndicies[j] );
 	}
 }
 
 /**Moves verticies of the new mesh to appropriate positions.*/
-void move_verticies( IndexedGeometry& mesh, std::vector<unsigned short>& edges, IndexedGeometry& resultMesh )
+void moveVerticies( IndexedGeometry& mesh, std::vector<unsigned short>& edges, IndexedGeometry& resultMesh )
 {
 	mesh;
 	edges;
--- BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperSmoothMesh.h
+++ BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperSmoothMesh.h
@@ -1,35 +1,10 @@
 #pragma once
 
-#include <vector>
-#include "Mathematics/glm_inc.h"
+
+#include "IndexedGeometry.h"
 
 namespace bv { namespace model {
 
-
-class IndexedGeometry
-{
-private:
-	std::vector<unsigned short>		indicies;
-	std::vector<glm::vec3>			verticies;
-public:
-	IndexedGeometry() {}
-	~IndexedGeometry() {}
-	IndexedGeometry( IndexedGeometry&& geometry ) : indicies( std::move( geometry.indicies) ), verticies( std::move( geometry.verticies ) ) {}
-
-	IndexedGeometry& operator=( IndexedGeometry&& geometry )
-	{
-		if( this != &geometry )
-		{
-			indicies = std::move( geometry.indicies );
-			verticies = std::move( geometry.verticies );
-		}
-
-		return *this;
-	}
-
-	std::vector<unsigned short>& getIndicies() { return indicies; }
-	std::vector<glm::vec3>& getVerticies() { return verticies; }
-};
 
 
 class HelperSmoothMesh
@@ -37,15 +12,16 @@ class HelperSmoothMesh
 private:
 
 protected:
-	void private_smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh );
+	void privateSmooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh );
 	void tesselate( IndexedGeometry& mesh, IndexedGeometry& resultMesh );
-	void move_verticies( IndexedGeometry& mesh, std::vector<unsigned short>& edges, IndexedGeometry& resultMesh );
+	void moveVerticies( IndexedGeometry& mesh, std::vector<unsigned short>& edges, IndexedGeometry& resultMesh );
 
 public:
 	HelperSmoothMesh(void);
 	~HelperSmoothMesh(void);
 
 	void smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh );
+	IndexedGeometry smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation );
 };
 
 } //model
