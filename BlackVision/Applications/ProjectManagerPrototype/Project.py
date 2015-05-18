46b6091db5c5d3225bc74a7a407e2fcd781b3a74
Radek Zagorowicz <radek.zagorowicz@gmail.com>
2015-05-18 22:22:36 +0200

    in progress. don't ask.

--- BlackVision/Applications/ProjectManagerPrototype/Project.py
+++ BlackVision/Applications/ProjectManagerPrototype/Project.py
@@ -15,7 +15,7 @@ class Project:
     def appendAsset(self, type, path):
         assert isinstance(type, PMAssetType)
         assert isinstance(path, [str])
-
+        assert False  # TODO: Implement
 
     def __str__(self):
         print("Project {} \n\t".format(self.name))
@@ -24,4 +24,5 @@ class Project:
 
     def getAssetEntity(self, location):
         assert isinstance(location, PMLocation)
+        assert False  # TODO: Implement
 
