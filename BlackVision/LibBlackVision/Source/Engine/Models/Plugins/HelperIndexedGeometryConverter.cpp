77bb4ee67b6ef0e1172ba6b00c8a31a8b6a2637c
WitekD <WitekD@localhost>
2015-05-18 14:44:03 +0200

    Geometry converter - skeleton

--- BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperIndexedGeometryConverter.cpp
+++ BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperIndexedGeometryConverter.cpp
@@ -12,6 +12,107 @@ IndexedGeometryConverter::~IndexedGeometryConverter(void)
 {
 }
 
+/**Function convert indexed triangle list to strips and ads verticies to verts param.
+Function doesn't take into account sharp edges. It will be in future versions.
+
+@param[in] mesh Mesh to convert.
+@param[out] verts Object to store result verticies.*/
+void IndexedGeometryConverter::makeStrip( IndexedGeometry& mesh, Float3AttributeChannelPtr verts)
+{
+	usedRangeIndex = 0;
+
+	std::vector<unsigned short>& indicies = mesh.getIndicies();
+	std::vector<glm::vec3>& verticies = mesh.getVerticies();
+	if( indicies.size() < 3 )
+		return;
+
+	remainnigTriangles = indicies.size() / 3;
+	usedTriangles.resize( remainnigTriangles, false );
+
+
+	unsigned short lastIndex1;
+	unsigned short lastIndex2;
+	unsigned short newIndex;
+
+
+	while( remainnigTriangles )
+	{
+		addFirstUnusedPoints( lastIndex1, lastIndex2, indicies, verticies, verts );
+
+		while( findNeighbour( lastIndex1, newIndex, indicies, verticies ) || findNeighbour( lastIndex2, newIndex, indicies, verticies ) )
+		{
+
+			while( remainnigTriangles && findNeighbour( lastIndex1, lastIndex2, newIndex, indicies, verticies ) )
+			{
+				verts->AddAttribute( verticies[ indicies[ newIndex ] ] );
+				lastIndex1 = lastIndex2;
+				lastIndex2 = newIndex;
+
+				--remainnigTriangles;
+			}
+
+			if( remainnigTriangles )
+				verts->AddAttribute( verticies[ indicies[ newIndex ] ] );
+		}
+
+		if( remainnigTriangles )
+		{
+			verts->AddAttribute( verticies[ indicies[ newIndex ] ] );
+			verts->AddAttribute( verticies[ indicies[ newIndex ] ] );
+		}
+	}
+
+	usedTriangles.resize( 0, false );
+}
+
+/**Finds neighbour for triangle, that consists of verticies pointed by indicies pointed by
+parameters!!!.
+Theese parameters aren't indicies to vertex table, but to index table.
+The same for return index;
+
+If next index is found, whole triangle is set as used in usedIndicies table.
+
+@param[in] index1 First index of index of vertex.
+@param[in] index1 Second index of index of vertex.
+return Returns false if couldn't find traingle having such indicies.
+*/
+bool IndexedGeometryConverter::findNeighbour( unsigned short index1, unsigned short index2, unsigned short& foundIndex, std::vector<unsigned short>& indicies, std::vector<glm::vec3>& verticies )
+{
+	index1;
+	index2;
+	foundIndex;
+	indicies;
+	verticies;
+	return false;
+}
+
+/**Finds neighbour as the second findNeighbour function, but looks for only one common vertex.*/
+bool IndexedGeometryConverter::findNeighbour( unsigned short index1, unsigned short& foundIndex, std::vector<unsigned short>& indicies, std::vector<glm::vec3>& verticies )
+{
+	index1;
+	foundIndex;
+	indicies;
+	verticies;
+	return false;
+}
+
+void IndexedGeometryConverter::addFirstUnusedPoints( unsigned short& index1, unsigned short& index2, std::vector<unsigned short>& indicies, std::vector<glm::vec3>& verticies, Float3AttributeChannelPtr verts )
+{
+	unsigned int i = usedRangeIndex;
+	for( ; i < indicies.size(); i += 3 )
+		if( !usedTriangles[i/3] )
+			break;
+
+	verts->AddAttribute( verticies[ indicies[i] ] );
+	verts->AddAttribute( verticies[ indicies[i+1] ] );
+	verts->AddAttribute( verticies[ indicies[i+2] ] );
+
+	//usedRangeIndex = 3;
+	usedTriangles[i/3] = true;
+
+	index1 = static_cast<unsigned short>( i+1 );
+	index2 = static_cast<unsigned short>( i+2 );
+}
 
 }
 
--- BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperIndexedGeometryConverter.h
+++ BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperIndexedGeometryConverter.h
@@ -10,8 +10,13 @@ namespace bv { namespace model {
 class IndexedGeometryConverter
 {
 private:
-
-
+	unsigned int			usedRangeIndex;		///< Points element in index table, that is the first unused element.
+	unsigned int			remainnigTriangles;	///< Number of traingles to use.
+	std::vector<bool>		usedTriangles;		///< Indicates which indicies(triangles) have been used.
+protected:
+	bool findNeighbour( unsigned short index1, unsigned short index2, unsigned short& foundIndex, std::vector<unsigned short>& indicies, std::vector<glm::vec3>& verticies );
+	bool findNeighbour( unsigned short index1, unsigned short& foundIndex, std::vector<unsigned short>& indicies, std::vector<glm::vec3>& verticies );
+	void addFirstUnusedPoints( unsigned short& index1, unsigned short& index2, std::vector<unsigned short>& indicies, std::vector<glm::vec3>& verticies, Float3AttributeChannelPtr verts );
 public:
 	IndexedGeometryConverter(void);
 	~IndexedGeometryConverter(void);
