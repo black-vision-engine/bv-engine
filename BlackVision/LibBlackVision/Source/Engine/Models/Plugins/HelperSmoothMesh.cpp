76951a62756363eb17ccc25eff9cd9d6a21dbcac
WitekD <WitekD@localhost>
2015-05-18 11:02:37 +0200

    HelperSmoothMesh - begin

---
+++ BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperSmoothMesh.cpp
@@ -0,0 +1,73 @@
+#include "HelperSmoothMesh.h"
+
+
+namespace bv { namespace model {
+
+
+
+HelperSmoothMesh::HelperSmoothMesh(void)
+{
+}
+
+
+HelperSmoothMesh::~HelperSmoothMesh(void)
+{
+}
+
+/**This is main function, that should be called. It tmake given mesh smooth using number of tesselations in param.
+
+Function calls private smooth to protect user from hurt himself. If he called private_smooth function with
+tesselation = 0, function would move content of mesh to resultMesh, leaving mesh in bad shape.
+@param[in] mesh Begin mesh to smooth.
+@param[in] edges Edges which should remain sharp.
+@param[in] tesselation Number of tesselations, that will be aplied to mesh
+@param[out] resultMesh Result of smooth.*/
+void HelperSmoothMesh::smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh )
+{
+	if( tesselation == 0 )
+		resultMesh = mesh;		// Normal copy assignment.
+	private_smooth( mesh, edges, tesselation, resultMesh );
+}
+
+/**This the real smooth function. Look at the description of smooth, if you want to know more.
+
+@param[in] mesh Begin mesh to smooth.
+@param[in] edges Edges which should remain sharp.
+@param[in] tesselation Number of tesselations, that will be aplied to mesh
+@param[out] resultMesh Result of smooth.*/
+void HelperSmoothMesh::private_smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh )
+{
+	if( tesselation == 0 )	// End of recursion.
+	{
+		resultMesh = std::move( mesh );		// We don't need mesh anymore so we move it, instead of coping it.
+		return;
+	}
+
+	IndexedGeometry new_mesh( mesh );
+
+
+	smooth( new_mesh, edges, tesselation - 1, resultMesh );
+	// Remember! You can't do anything with new_mesh after smooth call. It have been already moved to resultMesh.
+}
+
+/**Tesselates given mesh one time.*/
+void HelperSmoothMesh::tesselate( IndexedGeometry& mesh, IndexedGeometry& resultMesh )
+{
+	mesh;
+	resultMesh;
+	std::vector<unsigned short> indicies = mesh.getIndicies();
+	std::vector<glm::vec3> move_verticies = mesh.getVerticies();
+
+	//for( auto i = 0; i < 
+}
+
+/**Moves verticies of the new mesh to appropriate positions.*/
+void move_verticies( IndexedGeometry& mesh, IndexedGeometry& resultMesh )
+{
+	mesh;
+	resultMesh;
+}
+
+
+}	//model
+}	//bv
---
+++ BlackVision/LibBlackVision/Source/Engine/Models/Plugins/HelperSmoothMesh.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <vector>
+#include "Mathematics/glm_inc.h"
+
+namespace bv { namespace model {
+
+
+class IndexedGeometry
+{
+private:
+	std::vector<unsigned short>		indicies;
+	std::vector<glm::vec3>			verticies;
+public:
+	IndexedGeometry() {}
+	~IndexedGeometry() {}
+	IndexedGeometry( IndexedGeometry&& geometry ) : indicies( std::move( geometry.indicies) ), verticies( std::move( geometry.verticies ) ) {}
+
+	IndexedGeometry& operator=( IndexedGeometry&& geometry )
+	{
+		if( this != &geometry )
+		{
+			indicies = std::move( geometry.indicies );
+			verticies = std::move( geometry.verticies );
+		}
+
+		return *this;
+	}
+
+	std::vector<unsigned short>& getIndicies() { return indicies; }
+	std::vector<glm::vec3>& getVerticies() { return verticies; }
+};
+
+
+class HelperSmoothMesh
+{
+private:
+
+protected:
+	void private_smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh );
+	void tesselate( IndexedGeometry& mesh, IndexedGeometry& resultMesh );
+	void move_verticies( IndexedGeometry& mesh, IndexedGeometry& resultMesh );
+
+public:
+	HelperSmoothMesh(void);
+	~HelperSmoothMesh(void);
+
+	void smooth( IndexedGeometry& mesh, std::vector<unsigned short>& edges, unsigned int tesselation, IndexedGeometry& resultMesh );
+};
+
+} //model
+} //bv
