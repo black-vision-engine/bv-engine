34450400aaa06db1428180b89fcb13e44f2c21bd
creed <creed@localhost>
2015-05-19 12:17:32 +0200

    forgotten files

---
+++ BlackVision/LibBlackVision/Source/Engine/SerializationObjects.h
@@ -0,0 +1,24 @@
+#pragma once
+
+//#include "Xml/RapidXml/RapidXml.hpp"
+//
+//namespace bv
+//{
+//
+//class SerializeObject
+//{
+//    rapidxml::xml_document<> m_doc;
+//
+//public:
+//    SerializeObject( rapidxml::xml_document<> doc ) : m_doc( doc ) { }
+//};
+//
+//class DeserializeObject
+//{
+//    rapidxml::xml_document<> m_doc;
+//
+//public:
+//    DeserializeObject( rapidxml::xml_document<> doc ) : m_doc( doc ) { }
+//};
+
+//}
