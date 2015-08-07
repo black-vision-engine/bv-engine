e7a686f3a110845fc46b5c71377c1433ce2457de
creed <creed@localhost>
2015-08-07 15:14:27 +0200

    Bezier interpolation, finally :)

--- BlackVision/LibBlackVision/Source/Mathematics/Interpolators/CompositeBezierInterpolator.cpp
+++ BlackVision/LibBlackVision/Source/Mathematics/Interpolators/CompositeBezierInterpolator.cpp
@@ -26,6 +26,50 @@ public:
     }
 };
 
+template< class TimeValueT, class ValueT >
+class BezierEvaluator : public IInterpolator< TimeValueT, ValueT >
+{
+    typedef Key< TimeValueT, ValueT > Key;
+
+    Key key1, key2;
+    Key v1, v2;
+public:
+    BezierEvaluator( Key k1, Key k2, Key v1_, Key v2_ ) : key1( k1 ), key2( k2 ), v1( v1_ ), v2( v2_ ) {}
+    ValueT Evaluate( TimeValueT t ) const override 
+    {
+        Key A = key1,
+            B = key1 + v1,
+            C = key2 + v2,
+            D = key2;
+
+        for( ; ; ) 
+        {
+            Key middle = 1./8 * A + 3./8 * B + 3./8 * C + 1./8 * D;
+            
+            if( fabs( middle.t - t ) < 0.0001 ) // FIXME!!!
+                return middle.val;
+            else
+            {
+                if( t < middle.t )
+                {
+                    A = A;
+                    B = 1./2 * A + 1./2 * B;
+                    C = 1./4 * A + 1./2 * B + 1./4 * C;
+                    D = middle;
+                }
+                else
+                {
+                    A = middle;
+                    B = 1./4 * B + 1./2 * C + 1./4 * D;
+                    C = 1./2 * C + 1./2 * D;
+                    D = D;               
+                }
+            }
+        }
+    }
+};
+
+
 void CompositeBezierInterpolator::AddKey             ( TimeValueT t, const ValueT & v ) 
 { 
     if( keys.size() > 0 && keys[ keys.size()-1 ].t == t )
@@ -35,13 +79,11 @@ void CompositeBezierInterpolator::AddKey             ( TimeValueT t, const Value
     keys.push_back( Key<TimeValueT, ValueT>( t, v ) ); // FIXME sortme
     if( keys.size() > 1 )
         //interpolators.push_back( new ConstEvaluator< TimeValueT, ValueT >( v ) );
-        interpolators.push_back( new LinearEvaluator< TimeValueT, ValueT >( keys[ keys.size()-2 ], keys[ keys.size()-1 ] ) );
+        //interpolators.push_back( new LinearEvaluator< TimeValueT, ValueT >( keys[ keys.size()-2 ], keys[ keys.size()-1 ] ) );
+        interpolators.push_back( new BezierEvaluator< TimeValueT, ValueT >( keys[ keys.size()-2 ], keys[ keys.size()-1 ], Key<TimeValueT, ValueT>( 0, 0 ), Key<TimeValueT, ValueT>( 0, 0 ) ) );
 }
 
-float CompositeBezierInterpolator::PreEvaluate( float /*t*/ ) const 
-{ 
-    return keys[ 0 ].val; // never FIXME :P
-}
+float CompositeBezierInterpolator::PreEvaluate( float /*t*/ ) const { return keys[ 0 ].val; } // never FIXME :P
 
 float CompositeBezierInterpolator::PostEvaluate( float /*t*/ ) const { return keys[ keys.size()-1 ].val; } // never FIXME :P
 
