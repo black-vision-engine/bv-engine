c07292447c4424839637d54ab108d8d4351f413e
Radek Zagorowicz <radoslaw.zagorowicz@imapp.pl>
2015-08-20 17:34:08 +0200

    AssetCategory interface implemented

---
+++ BlackVision/Applications/BlackVision/Source/ProjectManager/AssetCategory.h
@@ -0,0 +1,30 @@
+#include "NormalizedPath.h"
+
+namespace bv
+{
+
+class AssetDesc;
+class AssetAccessor;
+
+class AssetCategory
+{
+private:
+
+	const std::string		m_id;
+	AssetAccessor *			m_assetAccessor;
+
+public:
+
+	const std::string &		GetId		() const;
+
+	void					AddAsset	( const NormalizedPath & path, const AssetDesc * assetDesc );
+	const AssetDesc *		GetAssetDesc( const NormalizedPath & path ) const;
+	void					RemoveAsset	( const NormalizedPath & path );
+	void					ExportAsset	( const NormalizedPath & expAssetFilePath, const NormalizedPath & path ) const;
+	void					ImportAsset	( const NormalizedPath & impAssetFilePath, const NormalizedPath & toPath );
+	void					ExportAll	( const NormalizedPath & expAssetFilePath ) const;
+	void					ListAssets	( const NormalizedPath & projectName ) const;
+
+};
+
+} // bv
