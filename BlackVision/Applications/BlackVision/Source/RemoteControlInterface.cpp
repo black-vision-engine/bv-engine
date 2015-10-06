de8d552f9909a42d6b0e34d0f808c29bddc28d70
Radek Zagorowicz <radoslaw.zagorowicz@imapp.pl>
2015-10-06 15:55:55 +0200

    New PM functions supported by RemoteInterface

--- BlackVision/Applications/BlackVision/Source/RemoteControlInterface.cpp
+++ BlackVision/Applications/BlackVision/Source/RemoteControlInterface.cpp
@@ -493,13 +493,13 @@ void RemoteControlInterface::OnSceneStructure ( bv::IEventPtr evt )
 			node_ptr->GetModelNodeEditor()->DetachPlugin("text");
 			
         }
-        else if( evtStructure->command == L"LIST_PROJECTS" )
+        else if( evtStructure->command == L"LIST_PROJECTS_NAMES" )
         {
             auto pns = pm->ListProjectsNames();
 
             auto pList = ToJSONArray( pns );
 
-            SendOnSceneStructureResponse( evtStructure, "LIST_PROJECTS", "list", pList );
+            SendOnSceneStructureResponse( evtStructure, "LIST_PROJECTS_NAMES", "list", pList );
 
             // [czesio]
             // [{"name":"czesio", "scenes_count":123},{...},...]
@@ -556,7 +556,27 @@ void RemoteControlInterface::OnSceneStructure ( bv::IEventPtr evt )
 
             SendOnSceneStructureResponse( evtStructure, "SET_CURRENT_PROJECT", "status", "OK" );
         }
+        else if( evtStructure->command == L"LIST_PROJECTS" )
+        {
+            auto pns = pm->ListProjectsNames();
 
+            Json::Value list;
+
+            for( auto p : pns )
+            {
+                auto scenesCount = pm->ListScenesNames( p ).size();
+
+                Json::Value entry;
+                entry[ "name" ] = p.Str();
+                entry[ "scenes_count" ] = scenesCount;
+
+                list.append( entry );
+            }
+
+            SendOnSceneStructureResponse( evtStructure, "LIST_PROJECTS", "list", list );
+            // [czesio]
+            // [{"name":"czesio", "scenes_count":123},{...},...]
+        }
     }
 }
 
