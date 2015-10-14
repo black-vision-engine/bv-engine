82c4e519c0bb3523004ba97c7ad4c254204f65b1
viggith <viggith@fewsquares.com>
2015-10-14 15:59:59 +0200

    Rendering logic (1/2) structure pushed towards (1/2) global effect implementation

---
+++ BlackVision/Applications/BlackVision/Source/Rendering/Logic/NodeEffectRendering/DefaultEffectRenderLogic.cpp
@@ -0,0 +1,27 @@
+#include "DefaultEffectRenderLogic.h"
+
+
+namespace bv {
+
+// *********************************
+//
+DefaultEffectRenderLogic::DefaultEffectRenderLogic  ()
+{
+}
+
+// *********************************
+//
+DefaultEffectRenderLogic::~DefaultEffectRenderLogic ()
+{
+}
+
+// *********************************
+//
+void    DefaultEffectRenderLogic::Render            ( Renderer * renderer, SceneNode * node, OffscreenRenderLogic * offscreenRenderLogic )
+{
+    { renderer; }
+    { node; }
+    { offscreenRenderLogic; }
+}
+
+} //bv
---
+++ BlackVision/Applications/BlackVision/Source/Rendering/Logic/NodeEffectRendering/NodeMaskRenderLogic.cpp
@@ -0,0 +1,5 @@
+#include "NodeMaskRenderLogic.h"
+
+
+namespace bv {
+} //bv
