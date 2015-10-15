ee876806c57a6dbdddea546be9ada52a506ae774
WitekD <WitekD@localhost>
2015-10-15 15:32:49 +0200

    After merge fix

---
+++ BlackVision/LibBlackVision/Source/Serialization/ISerializer.h
@@ -0,0 +1,31 @@
+#pragma once
+
+
+#include <string>
+
+namespace bv {
+
+
+class ISerializer
+{
+private:
+protected:
+public:
+	virtual ~ISerializer();
+
+	virtual void				SetAttribute        ( const std::string& name, const std::string& value ) = 0;
+	virtual std::string			GetAttribute        ( const std::string& name ) = 0;
+
+    /**@brief Push.
+    @return If serializing - always true.
+    If deserializing, returns false if child does not exist.*/
+    virtual bool                EnterChild          ( const std::string& name ) = 0;
+    
+    /**@brief Pop.*/
+    virtual void                ExitChild           ( const std::string& name ) = 0;
+};
+
+
+
+};
+
