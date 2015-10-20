2d330e8325d299ae292ab0c68ac47f9f441ea03a
creed <creed@localhost>
2015-10-20 14:28:09 +0200

    warning fix

--- BlackVision/LibBlackVision/Source/Serialization/SerializationHelper.h
+++ BlackVision/LibBlackVision/Source/Serialization/SerializationHelper.h
@@ -3,8 +3,7 @@
 #include "ISerializable.h"
 #include "SerializationObjects.h"
 
-#include <vector>
-#include <sstream>
+#include "Mathematics/glm_inc.h"
 
 namespace bv {
 
@@ -38,47 +37,9 @@ std::shared_ptr< T > Create( ISerializer& sob )
 
 // glm stuff
 
-inline std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
-    std::stringstream ss(s);
-    std::string item;
-    while (std::getline(ss, item, delim)) {
-        elems.push_back(item);
-    }
-    return elems;
-}
-
-inline std::vector<std::string> split(const std::string &s, char delim) {
-    std::vector<std::string> elems;
-    split(s, delim, elems);
-    return elems;
-}
-
-glm::vec4               String2Vec4( std::string s )
-{
-    auto vals = SerializationHelper::split( s, ',' );
-    assert( vals.size() == 4 );
-    return glm::vec4( std::stof( vals[0] ), 
-        std::stof( vals[1] ), 
-        std::stof( vals[2] ), 
-        std::stof( vals[3] ) );
-}
-
-glm::vec3               String2Vec3( std::string s )
-{
-    auto vals = SerializationHelper::split( s, ',' );
-    assert( vals.size() == 3 );
-    return glm::vec3( std::stof( vals[0] ), 
-        std::stof( vals[1] ), 
-        std::stof( vals[2] ) );
-}
-
-glm::vec2               String2Vec2( std::string s )
-{
-    auto vals = SerializationHelper::split( s, ',' );
-    assert( vals.size() == 2 );
-    return glm::vec2( std::stof( vals[0] ), 
-        std::stof( vals[1] ) );
-}
+glm::vec4               String2Vec4( std::string s );
+glm::vec3               String2Vec3( std::string s );
+glm::vec2               String2Vec2( std::string s );
 
 }
 
