617ba9f84ddb8d68082fb49cc7faa1f799ec05dd
viggith <viggith@fewsquares.com>
2016-01-15 16:38:47 +0100

    Public access API for node effect (engine side) implemented

--- BlackVision/Applications/BlackVision/Source/Rendering/Logic/NodeEffect/NodeEffectTr.cpp
+++ BlackVision/Applications/BlackVision/Source/Rendering/Logic/NodeEffect/NodeEffectTr.cpp
@@ -1,5 +1,51 @@
 #include "NodeEffectTr.h"
 
+#include "Rendering/Logic/NodeEffect/NodeEffectLogic.h"
+
 
 namespace bv {
+
+// *********************************
+//
+NodeEffectTr::NodeEffectTr                ( NodeEffectLogic * logic )
+    : m_logic( logic )
+{
+    assert( m_logic );
+}
+
+// *********************************
+//
+NodeEffectTr::~NodeEffectTr               ()
+{
+    delete m_logic;
+}
+
+// *********************************
+//
+void            NodeEffectTr::Render                      ( SceneNode * node, RenderLogicContext * ctx )
+{
+    m_logic->Render( node, ctx );
+}
+
+// *********************************
+//
+unsigned int    NodeEffectTr::GetNumValues                () const
+{
+    return m_logic->GetNumValues();
+}
+
+// *********************************
+//
+IValuePtr       NodeEffectTr::GetValueAt                  ( unsigned int i ) const
+{
+    return m_logic->GetValueAt( i );
+}
+
+// *********************************
+//
+IValuePtr       NodeEffectTr::GetValue                    ( const std::string & name ) const
+{
+    return m_logic->GetValue( name );
+}
+
 } //bv
--- BlackVision/Applications/BlackVision/Source/Rendering/Logic/NodeEffect/NodeEffectTr.h
+++ BlackVision/Applications/BlackVision/Source/Rendering/Logic/NodeEffect/NodeEffectTr.h
@@ -1,5 +1,31 @@
 #pragma once
 
+#include "Engine/Interfaces/IValue.h"
+
 
 namespace bv {
+
+class NodeEffectLogic;
+class SceneNode;
+class RenderLogicContext;
+
+class NodeEffectTr
+{
+private:
+
+    NodeEffectLogic *   m_logic;
+
+public:
+
+                    NodeEffectTr                ( NodeEffectLogic * logic );
+                    ~NodeEffectTr               ();
+
+    void            Render                      ( SceneNode * node, RenderLogicContext * ctx );
+
+    unsigned int    GetNumValues                () const;
+    IValuePtr       GetValueAt                  ( unsigned int i ) const;
+    IValuePtr       GetValue                    ( const std::string & name ) const;
+
+};
+
 } //bv
