cbee3a30683e3fb68325edeb88474d5cd303a454
viggith <viggith@fewsquares.com>
2016-01-22 14:28:43 +0100

    Factory - empty implementation

--- BlackVision/LibBlackVision/Source/Engine/Graphics/Effects/NodeEffectTr/NodeEffectFactory.cpp
+++ BlackVision/LibBlackVision/Source/Engine/Graphics/Effects/NodeEffectTr/NodeEffectFactory.cpp
@@ -1,13 +1,69 @@
 #include "NodeEffectFactory.h"
 
+#include <cassert>
+
 
 namespace bv {
+
+namespace {
+
+// **************************
+//
+NodeEffectTr *  CreateDefaultNodeEffect()
+{
+    return nullptr;
+}
+
+// **************************
+//
+NodeEffectTr *  CreateAlphaMaskNodeEffect()
+{
+    return nullptr;
+}
+
+// **************************
+//
+NodeEffectTr *  CreateNodeMaskNodeEffect()
+{
+    return nullptr;
+}
+
+// **************************
+//
+NodeEffectTr *  CreateWireframeNodeEffect()
+{
+    return nullptr;
+}
+
+// **************************
+//
+NodeEffectTr *  CreateMixchannelsNodeEffect()
+{
+    return nullptr;
+}
+
+} // anonoymnous
+
 
 // **************************
 //
 NodeEffectTr *    CreateNodeEffect( NodeEffectType nodeEffectType )
 {
-    { nodeEffectType; }
+    switch( nodeEffectType )
+    {
+        case  NodeEffectType::NET_DEFAULT:
+            return CreateDefaultNodeEffect();
+        case NodeEffectType::NET_ALPHA_MASK:
+            return CreateAlphaMaskNodeEffect();
+        case NodeEffectType::NET_NODE_MASK:
+            return CreateNodeMaskNodeEffect();
+        case NodeEffectType::NET_WIREFRAME:
+            return CreateWireframeNodeEffect();
+        case NodeEffectType::NET_MIX_CHANNELS:
+            return CreateMixchannelsNodeEffect();
+        default:
+            assert( false );
+    }
 
     return nullptr;
 }
