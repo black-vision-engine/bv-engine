5ec7d3a6de180e928f08b1a815c82f729d3bb806
WitekD <WitekD@localhost>
2016-02-19 16:30:09 +0100

    Bug #113736609 probably fixed

--- BlackVision/LibBlackVision/Source/Serialization/BV/BVSerializeContext.h
+++ BlackVision/LibBlackVision/Source/Serialization/BV/BVSerializeContext.h
@@ -22,12 +22,14 @@ public:
         detailedInfo = true;
         pluginsInfo = true;
         extendedAssetData = false;
+        sceneNameInTimeline = true;
     }
 
     bool            recursive;
     bool            detailedInfo;
     bool            pluginsInfo;
     bool            extendedAssetData;
+    bool            sceneNameInTimeline;
 
     AssetDescsWithUIDsPtr               GetAssets               ();
     void                                SetAssets               ( const AssetDescsWithUIDsPtr & assets );
