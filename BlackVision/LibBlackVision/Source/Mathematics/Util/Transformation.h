817b4570b490083e9f2fd80314ca27cea26feba5
Radek Zagorowicz <radoslaw.zagorowicz@imapp.pl>
2016-03-20 16:02:31 +0100

    description added.

--- BlackVision/LibBlackVision/Source/Mathematics/Util/Transformation.h
+++ BlackVision/LibBlackVision/Source/Mathematics/Util/Transformation.h
@@ -7,9 +7,18 @@ namespace bv { namespace mathematics {
 class TransformationUtils
 {
 public:
-     static Box            Transform            ( const Box * box, const glm::mat4 & tx );
-     static Box            Project              ( const Box * box, const glm::mat4 & projMatrix );
-     static glm::vec2      ToScreenNormCoords   ( const glm::vec2 & screenPos );
+
+    // ******************************
+    // A silmple transformation of a box in a 3 demintional space.
+    static Box            Transform            ( const Box * box, const glm::mat4 & tx );
+
+    // ******************************
+    // A projection of a box onto a projection plane according to a projection matrix 'projMatrix'
+    static Box            Project              ( const Box * box, const glm::mat4 & projMatrix );
+
+    // ******************************
+    // A trasfomation of a projected point into a space ([0,1]x[0,1]) of displaing texture.
+    static glm::vec2      ToScreenNormCoords   ( const glm::vec2 & screenPos );
 };
 
 } // mathematics
