c7962334d0f0a72993ce2b4745af1f25ed30c032
WitekD <WitekD@localhost>
2016-05-17 10:40:38 +0200

    Cameras - first commit

---
+++ BlackVision/LibBlackVision/Source/Engine/Models/Cameras/CameraModel.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "CameraModel.h"
+
+
+
+namespace bv { namespace model {
+
+
+// ***********************
+//
+CameraModel::CameraModel()
+{}
+
+// ***********************
+//
+CameraModel::~CameraModel()
+{}
+
+
+} // model
+} //bv
---
+++ BlackVision/LibBlackVision/Source/Engine/Models/Cameras/CameraModel.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+
+
+namespace bv { namespace model {
+
+
+class CameraModel
+{
+private:
+public:
+    explicit CameraModel();
+    ~CameraModel();
+};
+
+
+} // model
+} //bv
