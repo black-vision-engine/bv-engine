d4601c0449944e4cea16f1d1d77bcefcaa1a146c
WitekD <WitekD@localhost>
2016-06-08 17:29:39 +0200

    Scenes thumbnail generation fixed

--- BlackVision/Applications/BlackVision/Source/EndUserAPI/EventHandlers/SceneEventsHandlers.cpp
+++ BlackVision/Applications/BlackVision/Source/EndUserAPI/EventHandlers/SceneEventsHandlers.cpp
@@ -459,7 +459,7 @@ void SceneEventsHandlers::ProjectStructure    ( bv::IEventPtr evt )
 
                         SendSimpleResponse( command, projectEvent->EventID, senderID, true );
                 
-                        RequestThumbnail( scene, newSceneName, ThumbnailType::Preset );
+                        RequestThumbnail( scene, newSceneName, ThumbnailType::Scene );
                     }
                     else
                     {
