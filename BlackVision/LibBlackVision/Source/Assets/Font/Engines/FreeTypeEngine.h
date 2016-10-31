44a293491d492506e54941104c2fc6022b507d28
Daria Kurpiewska <daria.kurpiewska@imapp.pl>
2016-10-31 19:33:59 +0100

    freetypeengine mem leak fixes

--- BlackVision/LibBlackVision/Source/Assets/Font/Engines/FreeTypeEngine.h
+++ BlackVision/LibBlackVision/Source/Assets/Font/Engines/FreeTypeEngine.h
@@ -1,52 +1,86 @@
 #pragma once
 
-
 #include "Assets/Font/IFontEngine.h"
-#include "Assets/Font/Text.h"
 
-#include "CoreDEF.h"
-
-#include <ft2build.h>
 #include FT_FREETYPE_H
 #include FT_STROKER_H
-#include "FTContour.h"
 
 
 namespace bv { 
 
-class TextAtlas;
 class FreeTypeEngine;
-struct Glyph;
-class Spans;
+DEFINE_CONST_PTR_TYPE( FreeTypeEngine )
+DEFINE_PTR_TYPE( FreeTypeEngine )
 
-DEFINE_CONST_PTR_TYPE(FreeTypeEngine)
-DEFINE_PTR_TYPE(FreeTypeEngine)
+
+struct GlyphDataInfo
+{
+    Glyph*          glyph;
+    unsigned int    pitch;
+    char*           data;
+
+    GlyphDataInfo   ( Glyph * g, unsigned int p, char * d );
+};
+
+
+struct Span
+{
+    Span    ();
+    Span    ( Int32 _x, Int32 _y, Int32 _width, Int32 _coverage );
+
+    Int32 x, y, width, coverage;
+};
+
+
+class Spans
+{
+public:
+
+    mathematics::Rect       m_boundingRect;
+    std::vector< Span * >   m_spans;
+
+public:
+
+    ~Spans();
+
+    Span *          operator []     ( SizeType i );
+    const Span *    operator []     ( SizeType i ) const;
+
+    SizeType        size            () const;
+    void            push_back       ( Span * s );
+    bool            empty           () const;
+
+};
+
 
 class FreeTypeEngine : public IFontEngine
 {
 private:
 
-	UInt32						m_maxHeight;
-	UInt32						m_maxWidth;
-	SizeType					m_fontSize;
-	FT_Library					m_library;
-	FT_Face 					m_face;
-	std::string					m_fontFilePath;
+    UInt32                      m_maxHeight;
+    UInt32                      m_maxWidth;
+    SizeType                    m_fontSize;
+    FT_Library                  m_library;
+    FT_Face                     m_face;
+    std::string                 m_fontFilePath;
 
-	Glyph *						RenderGlyph( wchar_t ch, Spans &, SizeType );
+    Glyph *                     RenderGlyph     ( wchar_t ch, Spans &, SizeType );
 
-								FreeTypeEngine( const std::string & fontFilePath, size_t fontSize );
+                                FreeTypeEngine  ( const std::string & fontFilePath, size_t fontSize );
 
 public:
-	virtual TextAtlasPtr	    CreateAtlas( UInt32 padding, const std::wstring & wcharsSet, bool makeSizesPowerOf2 = false )					override;
-	virtual TextAtlasPtr	    CreateAtlas( UInt32 padding, UInt32 outline, const std::wstring & wcharsSet, bool makeSizesPowerOf2 = false )	override;
 
-    virtual ContoursList        Create3dVerticies   ( wchar_t ch, float size )                                                                  override;
+    virtual TextAtlasPtr        CreateAtlas     ( UInt32 padding, const std::wstring & wcharsSet, bool makeSizesPowerOf2 = false ) override;
+    virtual TextAtlasPtr        CreateAtlas     ( UInt32 padding, UInt32 outline, const std::wstring & wcharsSet, bool makeSizesPowerOf2 = false ) override;
 
-	static FreeTypeEnginePtr	Create( const std::string & fontFilePath, size_t fontSize );
+    virtual ContoursList        Create3dVerticies   ( wchar_t ch, float size ) override;
+
+    static FreeTypeEnginePtr    Create          ( const std::string & fontFilePath, size_t fontSize );
 
 private:
-    ContoursList                MakeContours		( const FT_GlyphSlot glyph );
+
+    ContoursList                MakeContours    ( const FT_GlyphSlot glyph );
+
 };
 
 } // bv
--- BlackVision/LibBlackVision/Source/Assets/Font/IFontEngine.h
+++ BlackVision/LibBlackVision/Source/Assets/Font/IFontEngine.h
@@ -1,14 +1,7 @@
 #pragma once
 
 #include "TextAtlas.h"
-#include "CoreDEF.h"
-#include "Mathematics/glm_inc.h"
-
 #include "FTContour.h"
-
-#include <string>
-#include <vector>
-
 
 
 namespace bv { 
@@ -16,10 +9,12 @@ namespace bv {
 class IFontEngine
 {
 public:
-	virtual TextAtlasPtr	    CreateAtlas( UInt32 padding, const std::wstring & wcharsSet, bool makeSizesPowerOf2 = false )					 = 0;
-	virtual TextAtlasPtr	    CreateAtlas( UInt32 padding, UInt32 outline, const std::wstring & wcharsSet, bool makeSizesPowerOf2 = false )	 = 0;
+
+	virtual TextAtlasPtr	    CreateAtlas         ( UInt32 padding, const std::wstring & wcharsSet, bool makeSizesPowerOf2 = false )					 = 0;
+	virtual TextAtlasPtr	    CreateAtlas         ( UInt32 padding, UInt32 outline, const std::wstring & wcharsSet, bool makeSizesPowerOf2 = false )	 = 0;
 
     virtual ContoursList        Create3dVerticies   ( wchar_t ch, float size )                                                                   = 0;
+
 };
 
 DEFINE_CONST_PTR_TYPE(IFontEngine)
