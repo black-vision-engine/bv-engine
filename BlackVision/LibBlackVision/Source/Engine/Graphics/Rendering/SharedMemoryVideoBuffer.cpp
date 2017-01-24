d1d34d81554fb98f1e08eb3860e9417cfa517098
viggith <viggith@fewsquares.com>
2017-01-24 18:57:26 +0100

    shared memory buffer

--- BlackVision/LibBlackVision/Source/Engine/Graphics/Rendering/SharedMemoryVideoBuffer.cpp
+++ BlackVision/LibBlackVision/Source/Engine/Graphics/Rendering/SharedMemoryVideoBuffer.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+
 #include "Tools/Profiler/HerarchicalProfiler.h"
 #include "SharedMemoryVideoBuffer.h"
 
---
+++ BlackVision/LibCore/Source/IO/SharedMemoryBuffer.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <string>
+
+// FIXME: this is so much of a hack
+#include "win_sock.h"
+
+#include "CoreDEF.h"
+
+
+namespace bv {
+
+class SharedMemoryBuffer;
+
+DEFINE_PTR_TYPE( SharedMemoryBuffer )
+DEFINE_CONST_PTR_TYPE( SharedMemoryBuffer )
+
+class SharedMemoryBuffer
+{
+
+private:
+
+    UInt32          m_bufferSize;
+
+    HANDLE          m_hMapFile;
+
+    LPCTSTR         m_pBuf;
+
+private:
+
+                                    SharedMemoryBuffer  ( HANDLE hMapFile, LPTSTR pBuf, UInt32 bufferSize );
+
+public:
+
+                                    ~SharedMemoryBuffer ();
+
+public:
+
+    void                            Write               ( const char * data, UInt32 size );
+
+public:
+
+    static  SharedMemoryBufferPtr   Create              ( const std::wstring & name, UInt32 bufferSize );
+
+};
+
+} //bv
