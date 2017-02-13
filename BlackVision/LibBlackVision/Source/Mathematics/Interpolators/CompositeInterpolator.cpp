d990ac8604ab006d7e9fce1290aa347481b734c7
rodia <radek.zagorowicz@gmail.com>
2017-02-13 15:30:35 +0100

    Fixes of interpolators.

--- BlackVision/LibBlackVision/Source/Mathematics/Interpolators/CompositeInterpolator.cpp
+++ BlackVision/LibBlackVision/Source/Mathematics/Interpolators/CompositeInterpolator.cpp
@@ -5,17 +5,17 @@
 namespace bv
 {
 
-//template class CompositeInterpolator<TimeType, TimeType>;
-//template class CompositeInterpolator<TimeType, bool>;
-//template class CompositeInterpolator<TimeType, int>;
-//template class CompositeInterpolator<TimeType, float>;
-//
-//template class CompositeInterpolator<TimeType, glm::vec2>;
-//template class CompositeInterpolator<TimeType, glm::vec3>;
-//template class CompositeInterpolator<TimeType, glm::vec4>;
-//
-//template class CompositeInterpolator< TimeType, std::string >;
-//template class CompositeInterpolator< TimeType, std::wstring >;
+template class CompositeInterpolator<TimeType, TimeType>;
+template class CompositeInterpolator<TimeType, bool>;
+template class CompositeInterpolator<TimeType, int>;
+template class CompositeInterpolator<TimeType, float>;
+
+template class CompositeInterpolator<TimeType, glm::vec2>;
+template class CompositeInterpolator<TimeType, glm::vec3>;
+template class CompositeInterpolator<TimeType, glm::vec4>;
+
+template class CompositeInterpolator< TimeType, std::string >;
+template class CompositeInterpolator< TimeType, std::wstring >;
 
 
 } // bv
