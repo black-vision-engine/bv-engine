0bddb9fcec1bf8b4a2f0a733dc79689e34c9d504
viggith <viggith@fewsquares.com>
2017-02-15 17:31:45 +0100

    Intermediate AVFrame container implemented

--- BlackVision/LibBlackVision/Source/Engine/Graphics/Effects/nrl/Logic/OutputRendering/Impl/CompositeOutputs/Components/AVOutputsData.cpp
+++ BlackVision/LibBlackVision/Source/Engine/Graphics/Effects/nrl/Logic/OutputRendering/Impl/CompositeOutputs/Components/AVOutputsData.cpp
@@ -1,9 +1,27 @@
 #include "stdafx.h"
 
+#include <cassert>
+
 #include "AVOutputsData.h"
 
 
 namespace bv { namespace nrl {
    
+// *********************************
+//
+AVFramePtr  AVOutputsData::GetAVFrame  ( unsigned int videoOutputId )
+{
+    assert( m_frames.find( videoOutputId ) != m_frames.end() );
+
+    return m_frames[ videoOutputId ];
+}
+
+// *********************************
+//
+void        AVOutputsData::SetAVFrame  ( unsigned int videoOutputId, AVFramePtr frame )
+{
+    m_frames[ videoOutputId ] = frame;
+}
+
 } //bv
 } //nrl
