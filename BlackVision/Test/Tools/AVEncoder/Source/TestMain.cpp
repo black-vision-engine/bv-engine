2d6440dbb5e1acd9fb42a0db46f76a48f267e9e8
rodia <radoslaw.zagorowicz@imapp.pl>
2017-05-22 18:05:10 +0200

    unnecessary code removed.

--- BlackVision/Test/Tools/AVEncoder/Source/TestMain.cpp
+++ BlackVision/Test/Tools/AVEncoder/Source/TestMain.cpp
@@ -34,33 +34,6 @@ TEST( WriteFrames, WriteFrames )
     }
 
     enc.CloseStream();
-	//	/* prepare a dummy image */
-	//	/* Y */
-	//	for (auto y = 0; y < 288; y++) {
-	//		for (auto x = 0; x < c->width; x++) {
-	//			frame->m_videoData[0][y * frame->linesize[0] + x] = x + y + i * 3;
-	//		}
-	//	}
-	//	/* Cb and Cr */
-	//	for ( auto y = 0; y < h / 2; y++) {
-	//		for ( auto  x = 0; w / 2; x++) {
-	//			frame->m_videoData[1][y * frame->linesize[1] + x] = 128 + y + i * 2;
-	//			frame->m_videoData[2][y * frame->linesize[2] + x] = 64 + x + i * 5;
-	//		}
-	//	}
-	//	frame->m_frameTime = i;
-	//	/* encode the image */
-	//	ret = avcodec_encode_video2(c, &pkt, frame, &got_output);
-	//	if (ret < 0) {
-	//		fprintf(stderr, "Error encoding frame\n");
-	//		exit(1);
-	//	}
-	//	if (got_output) {
-	//		printf("Write frame %3d (size=%5d)\n", i, pkt.size);
-	//		fwrite(pkt.data, 1, pkt.size, f);
-	//		av_packet_unref(&pkt);
-	//	}
-	//}
 }
 
 int main( int argc, char **argv )
