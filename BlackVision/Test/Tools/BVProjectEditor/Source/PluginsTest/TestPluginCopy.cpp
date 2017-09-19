f93a50629c4cfca1662122cad1c7f87719fff2ea
Witek <nieznany.sprawiciel@gmail.com>
2017-09-19 12:21:32 +0200

    Test ParamValModel default timeline copy

--- BlackVision/Test/Tools/BVProjectEditor/Source/PluginsTest/TestPluginCopy.cpp
+++ BlackVision/Test/Tools/BVProjectEditor/Source/PluginsTest/TestPluginCopy.cpp
@@ -10,7 +10,7 @@
 
 
 // ***********************
-//
+// Copied plugin should have the same timelines set as ancestor.
 SIMPLE_FRAMEWORK_TEST_IN_SUITE( CopyTimelines, PluginCopy.OneScene )
 {
     auto editor = GetProjectEditor();
@@ -21,9 +21,10 @@ SIMPLE_FRAMEWORK_TEST_IN_SUITE( CopyTimelines, PluginCopy.OneScene )
     CHECK( scene != nullptr );
 
     CHECK( editor->AddTimeline( scene->GetName(), "ColorTimeline", bv::TimelineType::TT_OFFSET ) );
-    CHECK( editor->AddTimeline( scene->GetName(), "TransformTimeline", bv::TimelineType::TT_OFFSET ) );
+    CHECK( editor->AddTimeline( scene->GetName(), "ParamValModelTimeline", bv::TimelineType::TT_OFFSET ) );
 
     auto colorTimeline = editor->GetTimeEvaluator( bv::model::TimelineHelper::CombineTimelinePath( scene->GetName(), "ColorTimeline" ) );
+    auto paramValModelTimeline = editor->GetTimeEvaluator( bv::model::TimelineHelper::CombineTimelinePath( scene->GetName(), "ParamValModelTimeline" ) );
 
     auto destNode = QueryTyped( editor->GetNode( scene->GetName(), "root/Group1" ) );
 
@@ -33,9 +34,17 @@ SIMPLE_FRAMEWORK_TEST_IN_SUITE( CopyTimelines, PluginCopy.OneScene )
     auto colorParam = colorPlugin->GetParameter( "color" );
     colorParam->SetTimeEvaluator( colorTimeline );
 
+    std::static_pointer_cast< bv::model::DefaultPluginParamValModel >( colorPlugin->GetPluginParamValModel() )->SetTimeEvaluator( paramValModelTimeline );
+
     auto plugin = editor->AddPluginCopy( scene, destNode, 2, scene, coloredRect, "solid color" );
 
+    // Parameters should have the same timeline as ancestor.
     CHECK( plugin->GetParameter( "color" )->GetTimeEvaluator() == colorTimeline );
+
+    // Parameters should have default timeline if no one changed it.
     CHECK( plugin->GetParameter( bv::model::BlendHelper::PARAM::BLEND_ENABLE )->GetTimeEvaluator() == editor->GetSceneDefaultTimeline( scene ) );
+
+    // ParamValModel timeline should have default timeline set to proper value
+    CHECK( std::static_pointer_cast< bv::model::DefaultPluginParamValModel >( plugin->GetPluginParamValModel() )->GetTimeEvaluator() == paramValModelTimeline );
 }
 
