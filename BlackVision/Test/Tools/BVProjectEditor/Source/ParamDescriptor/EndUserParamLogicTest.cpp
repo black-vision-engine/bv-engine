9f587de63f6dbbe8d60c511ecd5c4416e3604e5f
Witek <nieznany.sprawiciel@gmail.com>
2017-09-26 12:43:45 +0200

    Add and remove param descriptor test for all params types

--- BlackVision/Test/Tools/BVProjectEditor/Source/ParamDescriptor/EndUserParamLogicTest.cpp
+++ BlackVision/Test/Tools/BVProjectEditor/Source/ParamDescriptor/EndUserParamLogicTest.cpp
@@ -11,36 +11,161 @@ using namespace bv;
 
 // ***********************
 // Tests adding and removing parameter descriptor.
-SIMPLE_FRAMEWORK_TEST_IN_SUITE( BVProjectEditor.ParameterDescriptor, EndUserParamsLogic_AddRemovePluginParamDesc )
+SIMPLE_FRAMEWORK_TEST_IN_SUITE( BVProjectEditor.ParameterDescriptor, EndUserParamsLogic_AddRemoveParamDesc )
 {
-    auto scene = CreateOneSceneWithColoredRect( GetProjectEditor(), "Scene" );
+    auto scene = CreateSceneForParamDesc( GetProjectEditor(), "Scene" );
     EndUserParamsLogic paramsLogic( scene.get() );
 
     EndUserParamDescriptor desc;
-    desc.SetName( "Descr" );
+    desc.SetName( "PluginDesc" );
     desc.SetDescription( "This is descriptor" );
 
-    ParameterAddress address;
-    address.SceneName = scene->GetName();
-    address.NodeName = "root/ColoredRect";
-    address.ParamTargetType = ParameterAddress::PluginParam;
-    address.PluginName = "solid color";
-    address.ParamName = "color";
 
-    CHECK( paramsLogic.AddDescriptor( ParameterAddress( address ), std::move( desc ) ) );
+    // ***********************
+    // Add Plugin parameter descriptor.
+    ParameterAddress addressPlug;
+    addressPlug.SceneName = scene->GetName();
+    addressPlug.NodeName = "root/ColoredRect";
+    addressPlug.ParamTargetType = ParameterAddress::PluginParam;
+    addressPlug.PluginName = "solid color";
+    addressPlug.ParamName = "color";
 
-    auto returnDesc = paramsLogic.GetDescriptor( address );
+    CHECK( paramsLogic.AddDescriptor( ParameterAddress( addressPlug ), EndUserParamDescriptor( desc ) ) );
+
+    auto returnDesc = paramsLogic.GetDescriptor( addressPlug );
     REQUIRE CHECK( returnDesc != nullptr );
 
-    CHECK( returnDesc->GetName() == "Descr" );
+    CHECK( returnDesc->GetName() == "PluginDesc" );
     CHECK( returnDesc->GetDescription() == "This is descriptor" );
 
-    CHECK( paramsLogic.RemoveDescriptor( address ) );
+    // ***********************
+    // Add Resource param descriptor
+    ParameterAddress addressRes;
+    addressRes.SceneName = scene->GetName();
+    addressRes.NodeName = "root/TexturedRect";
+    addressRes.ParamTargetType = ParameterAddress::ResourceParam;
+    addressRes.PluginName = "texture";
+    addressRes.ParamName = "wrapModeX";
+    addressRes.ParamSubName = "Tex0";
 
-    returnDesc = paramsLogic.GetDescriptor( address );
-    CHECK( returnDesc == nullptr );
+    desc.SetName( "ResourceDesc" );
+
+    // Add
+    CHECK( paramsLogic.AddDescriptor( ParameterAddress( addressRes ), EndUserParamDescriptor( desc ) ) );
+
+    returnDesc = paramsLogic.GetDescriptor( addressRes );
+    REQUIRE CHECK( returnDesc != nullptr );
+
+    CHECK( returnDesc->GetName() == "ResourceDesc" );
+    CHECK( returnDesc->GetDescription() == "This is descriptor" );
+
+    // ***********************
+    // Add Light param descriptor
+    ParameterAddress addressLight;
+    addressLight.SceneName = scene->GetName();
+    addressLight.NodeName = "";
+    addressLight.ParamTargetType = ParameterAddress::LightParam;
+    addressLight.PluginName = "";
+    addressLight.ParamName = "color";
+    addressLight.ParamSubName = "";
+    addressLight.Index = 1;
+
+    desc.SetName( "LightDesc" );
+
+    // Add
+    CHECK( paramsLogic.AddDescriptor( ParameterAddress( addressLight ), EndUserParamDescriptor( desc ) ) );
+
+    returnDesc = paramsLogic.GetDescriptor( addressLight );
+    REQUIRE CHECK( returnDesc != nullptr );
+
+    CHECK( returnDesc->GetName() == "LightDesc" );
+    CHECK( returnDesc->GetDescription() == "This is descriptor" );
+
+    // ***********************
+    // Add Camera param descriptor
+    ParameterAddress addressCamera;
+    addressCamera.SceneName = scene->GetName();
+    addressCamera.NodeName = "";
+    addressCamera.ParamTargetType = ParameterAddress::CameraParam;
+    addressCamera.PluginName = "";
+    addressCamera.ParamName = "IsPerspective";
+    addressCamera.ParamSubName = "";
+    addressCamera.Index = 1;
+
+    desc.SetName( "CameraDesc" );
+
+    // Add
+    CHECK( paramsLogic.AddDescriptor( ParameterAddress( addressCamera ), EndUserParamDescriptor( desc ) ) );
+
+    returnDesc = paramsLogic.GetDescriptor( addressCamera );
+    REQUIRE CHECK( returnDesc != nullptr );
+
+    CHECK( returnDesc->GetName() == "CameraDesc" );
+    CHECK( returnDesc->GetDescription() == "This is descriptor" );
+
+    // ***********************
+    // Add Logic param descriptor
+    ParameterAddress addressLogic;
+    addressLogic.SceneName = scene->GetName();
+    addressLogic.NodeName = "root/TexturedRect";
+    addressLogic.ParamTargetType = ParameterAddress::NodeLogicParam;
+    addressLogic.PluginName = "";
+    addressLogic.ParamName = "OffsetX";
+    addressLogic.ParamSubName = "";
+    addressLogic.Index = 0;
+
+    desc.SetName( "LogicDesc" );
+
+    // Add
+    CHECK( paramsLogic.AddDescriptor( ParameterAddress( addressLogic ), EndUserParamDescriptor( desc ) ) );
+
+    returnDesc = paramsLogic.GetDescriptor( addressLogic );
+    REQUIRE CHECK( returnDesc != nullptr );
+
+    CHECK( returnDesc->GetName() == "LogicDesc" );
+    CHECK( returnDesc->GetDescription() == "This is descriptor" );
+
+    // ***********************
+    // Add Effect param descriptor
+    ParameterAddress addressEffect;
+    addressEffect.SceneName = scene->GetName();
+    addressEffect.NodeName = "root/ColoredRect";
+    addressEffect.ParamTargetType = ParameterAddress::GlobalEffectParam;
+    addressEffect.PluginName = "";
+    addressEffect.ParamName = "alpha";
+    addressEffect.ParamSubName = "";
+    addressEffect.Index = 0;
+
+    desc.SetName( "EffectDesc" );
+
+    // Add
+    CHECK( paramsLogic.AddDescriptor( ParameterAddress( addressEffect ), EndUserParamDescriptor( desc ) ) );
+
+    returnDesc = paramsLogic.GetDescriptor( addressEffect );
+    REQUIRE CHECK( returnDesc != nullptr );
+
+    CHECK( returnDesc->GetName() == "EffectDesc" );
+    CHECK( returnDesc->GetDescription() == "This is descriptor" );
+
+
+
+    // ***********************
+    // Remove phase
+    CHECK( paramsLogic.RemoveDescriptor( addressLight ) );
+    CHECK( paramsLogic.RemoveDescriptor( addressCamera ) );
+    CHECK( paramsLogic.RemoveDescriptor( addressEffect ) );
+    CHECK( paramsLogic.RemoveDescriptor( addressLogic ) );
+    CHECK( paramsLogic.RemoveDescriptor( addressPlug ) );
+    CHECK( paramsLogic.RemoveDescriptor( addressRes ) );
+
+    
+    CHECK( paramsLogic.GetDescriptor( addressLight ) == nullptr );
+    CHECK( paramsLogic.GetDescriptor( addressCamera ) == nullptr );
+    CHECK( paramsLogic.GetDescriptor( addressEffect ) == nullptr );
+    CHECK( paramsLogic.GetDescriptor( addressLogic ) == nullptr );
+    CHECK( paramsLogic.GetDescriptor( addressPlug ) == nullptr );
+    CHECK( paramsLogic.GetDescriptor( addressRes ) == nullptr );
 }
-
 
 
 
--- BlackVision/Test/Tools/BVProjectEditor/Source/Utils/TwoScenesFixture.h
+++ BlackVision/Test/Tools/BVProjectEditor/Source/Utils/TwoScenesFixture.h
@@ -2,8 +2,11 @@
 
 #include "UnitTest++.h"
 #include "Engine/Editors/BVProjectEditor.h"
+#include "Engine/Models/Timeline/TimelineHelper.h"
 
 #include "Utils/Nodes/TestNodesCreator.h"
+
+#include "Engine/Models/NodeLogics/Follow/Follow.h"
 
 
 
@@ -114,6 +117,35 @@ inline bv::model::SceneModelPtr     CreateOneSceneWithColoredRect       ( bv::BV
     
     editor->AddChildNode( scene, scene->GetRootNode(), node );
     editor->AddChildNode( scene->GetName(), "root", "Group1" );
+
+    return editor->GetModelScene( sceneName );
+}
+
+// ***********************
+//
+inline bv::model::SceneModelPtr     CreateSceneForParamDesc             ( bv::BVProjectEditor * editor, const std::string & sceneName )
+{
+    editor->AddScene( sceneName );
+
+    auto scene = editor->GetModelScene( sceneName );
+    auto timeline = editor->GetSceneDefaultTimeline( scene );
+
+    editor->AddLight( scene, bv::LightType::LT_POINT, timeline );
+    editor->AddLight( scene, bv::LightType::LT_DIRECTIONAL, timeline );
+
+    editor->AddCamera( scene );
+
+    auto node1 = bv::TestNodesCreator::ColoredRectangle( timeline, "ColoredRect", 10, 10, glm::vec4( 0.5f, 1.0, 1.0, 1.0 ) );
+    auto node2 = bv::TestNodesCreator::TexturedRectangle( timeline, "TexturedRect", 100, 100, "TestAssets/Common/checkerboard2_32X32.png" );
+
+    editor->AddChildNode( scene, scene->GetRootNode(), node1 );
+    editor->AddChildNode( scene, scene->GetRootNode(), node2 );
+    editor->AddChildNode( scene->GetName(), "root", "Group1" );
+
+    auto logic = std::make_shared< bv::nodelogic::Follow >( node2, timeline );
+    editor->SetLogic( node2, logic );
+
+    editor->SetNodeEffect( scene->GetName(), "root/ColoredRect", sceneName, "alpha mask" );
 
     return editor->GetModelScene( sceneName );
 }
