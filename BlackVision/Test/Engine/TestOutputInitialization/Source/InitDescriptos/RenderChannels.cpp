7d2a19438f734542fbb58cacbb8156282f37f242
Witek <nieznany.sprawiciel@gmail.com>
2017-10-03 14:41:21 +0200

    [Test] Missing RenderChannels description in config

--- BlackVision/Test/Engine/TestOutputInitialization/Source/InitDescriptos/RenderChannels.cpp
+++ BlackVision/Test/Engine/TestOutputInitialization/Source/InitDescriptos/RenderChannels.cpp
@@ -12,7 +12,7 @@ using namespace bv;
 
 // ***********************
 // Tests render channels initialization from config with proper content.
-TEST( Engine_RenderChannels, RenderLogicInitialization )
+TEST( Engine_RenderChannels, RenderLogicInit )
 {
     BVConfig config( "TestConfigs/OutputsTests/TwoRenderChannels.xml" );
 
@@ -25,3 +25,37 @@ TEST( Engine_RenderChannels, RenderLogicInitialization )
     EXPECT_FALSE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_3 ) );
     EXPECT_FALSE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_4 ) );
 }
+
+// ***********************
+// Tests initialization with missing entries in config file. Render channels are defaulted to false.
+TEST( Engine_RenderChannels, RenderLogicInitMissingEntries )
+{
+    BVConfig config( "TestConfigs/OutputsTests/MissingRenderChannelEntries.xml" );
+
+    RenderedChannelsDataDesc desc;
+
+    TEST_ACCESSOR( RenderLogicInitializer )::Initialize( desc, config );
+
+    EXPECT_TRUE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_1 ) );
+    EXPECT_FALSE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_2 ) );
+    EXPECT_FALSE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_3 ) );
+    EXPECT_FALSE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_4 ) );
+}
+
+// ***********************
+// Tests initialization without Description in config. First channel should be enabled by default.
+TEST( Engine_RenderChannels, RenderLogicInitWithoutConfig )
+{
+    BVConfig config( "TestConfigs/OutputsTests/NoRenderChannelsDesc.xml" );
+
+    RenderedChannelsDataDesc desc;
+
+    TEST_ACCESSOR( RenderLogicInitializer )::Initialize( desc, config );
+
+    EXPECT_TRUE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_1 ) );
+    EXPECT_FALSE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_2 ) );
+    EXPECT_FALSE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_3 ) );
+    EXPECT_FALSE( desc.IsEnabled( RenderChannelType::RCT_OUTPUT_4 ) );
+}
+
+
