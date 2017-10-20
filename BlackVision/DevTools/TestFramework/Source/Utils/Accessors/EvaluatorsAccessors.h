edff59254832ea550ca5cf4ecf7a86cc0ce9d4de
Witek <nieznany.sprawiciel@gmail.com>
2017-10-20 15:19:18 +0200

    [Test] Vec2, vec3, vec4 Serialize-Deserialize-Compare

--- BlackVision/DevTools/TestFramework/Source/Utils/Accessors/EvaluatorsAccessors.h
+++ BlackVision/DevTools/TestFramework/Source/Utils/Accessors/EvaluatorsAccessors.h
@@ -54,13 +54,71 @@ public:
 // Implementation
 // ========================================================================= //
 
+
+const float FloatLimit = 1.0e-6f;
+
+// ***********************
+//
+template< typename ParamType >
+inline bool         CompareEpsilon      ( const ParamType & param1, const ParamType & param2 )
+{
+    return param1 == param2;
+}
+
+// ***********************
+//
+template<>
+inline bool         CompareEpsilon< float >     ( const float & param1, const float & param2 )
+{
+    if( abs( param1 - param2 ) < FloatLimit )
+        return true;
+    return false;
+}
+
+// ***********************
+//
+template<>
+inline bool         CompareEpsilon< glm::vec2 >     ( const glm::vec2 & param1, const glm::vec2 & param2 )
+{
+    if( abs( param1.x - param2.x ) < FloatLimit &&
+        abs( param1.y - param2.y ) < FloatLimit )
+        return true;
+    return false;
+}
+
+// ***********************
+//
+template<>
+inline bool         CompareEpsilon< glm::vec3 >     ( const glm::vec3 & param1, const glm::vec3 & param2 )
+{
+    if( abs( param1.x - param2.x ) < FloatLimit &&
+        abs( param1.y - param2.y ) < FloatLimit &&
+        abs( param1.z - param2.z ) < FloatLimit )
+        return true;
+    return false;
+}
+
+// ***********************
+//
+template<>
+inline bool         CompareEpsilon< glm::vec4 >     ( const glm::vec4 & param1, const glm::vec4 & param2 )
+{
+    if( abs( param1.x - param2.x ) < FloatLimit &&
+        abs( param1.y - param2.y ) < FloatLimit &&
+        abs( param1.z - param2.z ) < FloatLimit &&
+        abs( param1.w - param2.w ) < FloatLimit )
+        return true;
+    return false;
+}
+
+
 // ***********************
 //
 template< typename ParamType >
 inline bool         operator!=          ( const Key< TimeType, ParamType > & key1, const Key< TimeType, ParamType > & key2 )
 {
     if( key1.t == key2.t &&
-        key1.val == key2.val )
+        CompareEpsilon< ParamType >( key1.val, key2.val ) )
         return false;
     return true;
 }
--- BlackVision/Test/Model/TestSerialization/Source/TestInterpolators/TestInterpolator.cpp
+++ BlackVision/Test/Model/TestSerialization/Source/TestInterpolators/TestInterpolator.cpp
@@ -158,5 +158,90 @@ TEST( Serialization_ParamValModel, Interpolators_CompositeInterpolator_SerDeserW
 }
 
 
+// ***********************
+// Test compares CompositeInterpolator evaluators serialization and deserialization.
+// This is hight level tests that checks only if result of the serialization is the same as original.
+TEST( Serialization_ParamValModel, Interpolators_CompositeInterpolator_SerDeserVec2 )
+{
+    CompositeInterpolator< TimeType, glm::vec2 > expected;
 
+    expected.AddKey( 0.0f, glm::vec2( 2.0, 3.0 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_ELASTIC_IN_BOUNCE );
+    expected.AddKey( 1.0f, glm::vec2( 22.0, 37.0 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_ELASTIC_OUT );
+    expected.AddKey( 3.0f, glm::vec2( 0.0, 0.00001 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_ELASTIC_OUT_BOUNCE );
+    expected.AddKey( 4.0f, glm::vec2( 0.0, -34.0 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_LINEAR );
+    expected.AddKey( 7.0f, glm::vec2( 22.34315325, 3.1231 ) );
+
+    Serialize( expected, "InterpolatorSerDeserVec2.xml" );
+
+    auto actual = Deserialize< CompositeInterpolator< TimeType, glm::vec2 > >( "InterpolatorSerDeserVec2.xml", "interpolator" );
+    EXPECT_TRUE( ParamComparator::CompareEvaluators( expected, *( actual.get() ) ) );
+
+    EXPECT_EQ( TEST_ACCESSOR( CompositeInterpolator )::GetEvaluators( actual.get() ).size(), 4 );
+}
+
+// ***********************
+// Test compares CompositeInterpolator evaluators serialization and deserialization.
+// This is hight level tests that checks only if result of the serialization is the same as original.
+TEST( Serialization_ParamValModel, Interpolators_CompositeInterpolator_SerDeserVec3 )
+{
+    CompositeInterpolator< TimeType, glm::vec3 > expected;
+
+    expected.AddKey( 0.0f, glm::vec3( 2.0, 3.0, 1.0 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_QUARTIC_INOUT );
+    expected.AddKey( 1.0f, glm::vec3( 22.0, 37.0, -15 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_ELASTIC_OUT_BOUNCE );
+    expected.AddKey( 3.0f, glm::vec3( 0.0, 0.00001, 0.1 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_CUBIC_IN );
+    expected.AddKey( 4.0f, glm::vec3( 0.0, -34.0, 13.00002 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_ELASTIC_IN );
+    expected.AddKey( 7.0f, glm::vec3( 22.34315325, 3.1231, 15.000006 ) );
+
+    Serialize( expected, "InterpolatorSerDeserVec3.xml" );
+
+    auto actual = Deserialize< CompositeInterpolator< TimeType, glm::vec3 > >( "InterpolatorSerDeserVec3.xml", "interpolator" );
+    EXPECT_TRUE( ParamComparator::CompareEvaluators( expected, *( actual.get() ) ) );
+
+    EXPECT_EQ( TEST_ACCESSOR( CompositeInterpolator )::GetEvaluators( actual.get() ).size(), 4 );
+}
+
+// ***********************
+// Test compares CompositeInterpolator evaluators serialization and deserialization.
+// This is hight level tests that checks only if result of the serialization is the same as original.
+TEST( Serialization_ParamValModel, Interpolators_CompositeInterpolator_SerDeserVec4 )
+{
+    CompositeInterpolator< TimeType, glm::vec4 > expected;
+
+    expected.AddKey( 0.0f, glm::vec4( 2.0, 3.0, 1.0, 0.0 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_COSINE_LIKE );
+    expected.AddKey( 1.0f, glm::vec4( 22.0, 37.0, -15, -11 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_ELASTIC_OUT_BOUNCE );
+    expected.AddKey( 3.0f, glm::vec4( 0.0, 0.00001, 0.1, -0.000012 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_QUARTIC_INOUT );
+    expected.AddKey( 4.0f, glm::vec4( 0.0, -34.0, 13.00002, 1.01111 ) );
+
+    expected.SetAddedKeyCurveType( CurveType::CT_BEZIER );
+    expected.AddKey( 7.0f, glm::vec4( 22.34315325, 3.1231, 15.000006, 0.9999999999 ) );
+
+    Serialize( expected, "InterpolatorSerDeserVec4.xml" );
+
+    auto actual = Deserialize< CompositeInterpolator< TimeType, glm::vec4 > >( "InterpolatorSerDeserVec4.xml", "interpolator" );
+    EXPECT_TRUE( ParamComparator::CompareEvaluators( expected, *( actual.get() ) ) );
+
+    EXPECT_EQ( TEST_ACCESSOR( CompositeInterpolator )::GetEvaluators( actual.get() ).size(), 4 );
+}
 
