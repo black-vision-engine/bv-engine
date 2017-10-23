e22b60902c2049302599a9ef9e7d8d4e6ee76f63
Witek <nieznany.sprawiciel@gmail.com>
2017-10-23 13:27:41 +0200

    [Test] No interpolations, no interpolation marker, lack of interpolatros

--- BlackVision/Test/Model/TestSerialization/Source/TestInterpolators/InvalidInterpolations.cpp
+++ BlackVision/Test/Model/TestSerialization/Source/TestInterpolators/InvalidInterpolations.cpp
@@ -34,3 +34,38 @@ TEST( Serialization_ParamValModel, Interpolators_MoreKeysThenInterpolators )
     EXPECT_EQ( actualEvals[ 3 ]->GetType(), EvaluatorType::ET_LINEAR );
 }
 
+// ***********************
+// More interpolators then needed for number of keys in xml.
+// Ignore all redundant interpolators.
+TEST( Serialization_ParamValModel, Interpolators_ToManyInterpolators )
+{
+    auto actual = Deserialize< CompositeInterpolator< TimeType, float > >( "TestAssets/Serialization/Interpolators/ToManyInterpolators.xml", "interpolator" );
+    ASSERT_NE( actual, nullptr );
+
+    auto & actualEvals = TEST_ACCESSOR( CompositeInterpolator )::GetEvaluators( actual.get() );
+    ASSERT_EQ( actualEvals.size(), 2 );
+}
+
+// ***********************
+// There're no interpolators in xml. We should create default interpolators.
+TEST( Serialization_ParamValModel, Interpolators_LackOfInterpolatorsInXML )
+{
+    auto actual = Deserialize< CompositeInterpolator< TimeType, float > >( "TestAssets/Serialization/Interpolators/LackOfInterpolators.xml", "interpolator" );
+    ASSERT_NE( actual, nullptr );
+
+    auto & actualEvals = TEST_ACCESSOR( CompositeInterpolator )::GetEvaluators( actual.get() );
+    ASSERT_EQ( actualEvals.size(), 2 );
+}
+
+
+// ***********************
+// There're no interpolations marker. We should create default interpolators.
+TEST( Serialization_ParamValModel, Interpolators_NoInterpolationsMarkerInXML )
+{
+    auto actual = Deserialize< CompositeInterpolator< TimeType, float > >( "TestAssets/Serialization/Interpolators/NoInteprolationsMarker.xml", "interpolator" );
+    ASSERT_NE( actual, nullptr );
+
+    auto & actualEvals = TEST_ACCESSOR( CompositeInterpolator )::GetEvaluators( actual.get() );
+    ASSERT_EQ( actualEvals.size(), 2 );
+}
+
