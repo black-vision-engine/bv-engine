04760fd93e2e94a39b7fe714a564d0da4072d8ec
Witek <nieznany.sprawiciel@gmail.com>
2017-11-23 16:16:33 +0100

    Mock audio plugin

---
+++ BlackVision/Test/Engine/TestAudioVideo/Source/AudioCardOut/TestAudioOutput.cpp
@@ -0,0 +1,17 @@
+#include "gtest/gtest.h"
+#include "Framework/FrameworkTest.h"
+
+#include "Utils/Mocks/FakeVideoCard/FakeVideoCard.h"
+
+
+
+using namespace bv;
+
+
+
+// ***********************
+//
+SIMPLE_FRAMEWORK_TEST_IN_SUITE( Engine_Audio, VideoCardsAudioOutput )
+{
+
+}
