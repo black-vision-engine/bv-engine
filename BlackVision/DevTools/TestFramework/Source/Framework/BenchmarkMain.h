12026b19e2dd981a804b304c66af095cbe5b29da
Witek <nieznany.sprawiciel@gmail.com>
2017-12-07 12:11:47 +0100

    Benchmark main

---
+++ BlackVision/DevTools/TestFramework/Source/Framework/BenchmarkMain.h
@@ -0,0 +1,35 @@
+#pragma once
+
+// ***********************
+//
+// Include this file in your application entry point main.cpp file.
+// Remember to include it only once in whole project.
+
+#include "benchmark/benchmark.h"
+
+#include "TestEnvironment.h"
+
+
+
+// ***********************
+//
+int main( int argc, char** argv )
+{
+    auto env = new bv::TestEnvironment( &argc, argv );
+    bv::TestEnvironment::SetEnvironment( env );
+
+    env->SetUp();
+
+    ::benchmark::Initialize( &argc, argv );
+    if( ::benchmark::ReportUnrecognizedArguments( argc, argv ) )
+    {
+        env->TearDown();
+        return 1;
+    }
+    
+    ::benchmark::RunSpecifiedBenchmarks();
+
+    env->TearDown();
+    return 0;
+}
+
