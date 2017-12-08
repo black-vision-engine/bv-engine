ec7e60a3cf6e6d7409e765ef8ea024c0a04df7a6
Witek <nieznany.sprawiciel@gmail.com>
2017-12-08 15:47:10 +0100

    MainLoop benchmark helper

--- BlackVision/Benchmarks/Engine/BenchmarkMultiScenes/Source/SceneBuffersOverheadBench.cpp
+++ BlackVision/Benchmarks/Engine/BenchmarkMultiScenes/Source/SceneBuffersOverheadBench.cpp
@@ -36,28 +36,9 @@ void            SceneBuffersOverhead  ( benchmark::State& state )
 {
     auto env = TestEnvironment::GetEnvironment();
 
-    // Always unload all scenes at the beginning of benchmark.
-    // There's no mechanims that can do that automatically.
-    env->GetAppLogic()->UnloadScenes();
-    
-    // Here goes initialization code of our benchmark.
     auto editor = env->GetAppLogic()->GetBVProject()->GetProjectEditor();
     CreateEmptyScenes( editor, state.range( 0 ) );
-
-    // Make one main loop step. In this step BV will create engine representation of model scenes
-    // and load all needed resource on GPU.
-    env->MainLoopStep();
-
-    for( auto _ : state )
-    {
-        env->MainLoopStep();
-    }
-
-    // Be good citizen of Framework and unload all scenes.
-    env->GetAppLogic()->UnloadScenes();
 }
 
-BV_BENCHMARK( SceneBuffersOverhead )->Arg( 30 )->Iterations( 2000 )->Repetitions( 20 );
 
-
-
+MAINLOOP_BENCHMARK( SceneBuffersOverhead )->Arg( 30 )->Iterations( 2000 )->Repetitions( 20 );
--- BlackVision/DevTools/TestFramework/Source/Framework/BenchmarkInclude.h
+++ BlackVision/DevTools/TestFramework/Source/Framework/BenchmarkInclude.h
@@ -9,3 +9,51 @@
 
 #include "Framework/Benchmark.h"
 
+
+
+
+namespace bv {
+namespace impl
+{
+
+typedef void ( *BenchmarkLoopInit )( benchmark::State& state );
+
+// ***********************
+//
+void            BenchmarkLoopImpl       ( benchmark::State& state, BenchmarkLoopInit initFun )
+{
+    auto env = TestEnvironment::GetEnvironment();
+
+    // Always unload all scenes at the beginning of benchmark to be sure that engine is in clean state. 
+    // Not everyone must use MAINLOOP_BENCHMARK macro.
+    env->GetAppLogic()->UnloadScenes();
+
+    // Call user defined initialization function.
+    initFun( state );
+
+    // Make one main loop step. In this step BV will create engine representation of model scenes
+    // and load all needed resource on GPU.
+    env->MainLoopStep();
+
+    // Meassured code goes inside this for loop.
+    for( auto _ : state )
+    {
+        env->MainLoopStep();
+    }
+
+    // Be good citizen of Framework and unload all scenes.
+    env->GetAppLogic()->UnloadScenes();
+}
+
+}
+
+
+}	// bv
+
+
+#define MAINLOOP_BENCHMARK( FunctionName )                              \
+void        FunctionName ## _LoopBench      ( benchmark::State& state ) \
+{    ::bv::impl::BenchmarkLoopImpl( state, &FunctionName );     }       \
+BV_BENCHMARK( FunctionName ## _LoopBench )
+
+
