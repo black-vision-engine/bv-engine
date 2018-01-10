c57e6810316224fe3ec3ece1ab832a8041231178
Witek <nieznany.sprawiciel@gmail.com>
2018-01-10 10:42:20 +0100

    [Test Moved] Fallback texture test moved to TexturePlugin test from TestBVProjectEditor

--- BlackVision/Test/Plugins/TestTexturePlugins/Source/TexturePluginBase/LoadResourceTest.cpp
+++ BlackVision/Test/Plugins/TestTexturePlugins/Source/TexturePluginBase/LoadResourceTest.cpp
@@ -7,6 +7,9 @@
 #include"Assets/Texture/TextureAssetDescriptor.h"
 
 #include "Utils/Scenes/TestScenesCreator.h"
+#include "Utils/Nodes/TestNodesCreator.h"
+
+#include "Assets/DefaultAssets.h"
 
 
 // ========================================================================= //
@@ -17,23 +20,42 @@
 using namespace bv;
 
 
-// ***********************
-// Note: Fallback texture loading test is placed in TestBVProjectEditor/AssetsTest
+extern const char* imagePath_32x32 = "TestAssets/BVProjectEditor/checkerboard2_32x32.png";;
 
+
+// ***********************
+// Providing path to non existing file should load fallback texture.
+SIMPLE_FRAMEWORK_TEST_IN_SUITE( Plugins_TexturePlugins, ResourceLoading_FallbackTexture )
+{
+    auto texturePlugin = TestScenesCreator::TexturedRectangle( GetProjectEditor(), "Scene", 400, 400, imagePath_32x32 );
+    auto desc = TextureAssetDesc::Create( "Plugins/TestTexturePlugins/NonExistingTexturePath.jpg", false );
+
+    ASSERT_NE( texturePlugin, nullptr );
+
+    // Loading fallback texture result in false return value.
+    EXPECT_TRUE( !texturePlugin->LoadResource( desc ) );
+
+    auto lassets = texturePlugin->GetLAssets();
+    ASSERT_TRUE( lassets.size() == 1 );
+
+    auto texDesc = std::static_pointer_cast< const TextureAssetDesc >( lassets[ 0 ].assetDesc );
+
+    EXPECT_NE( texDesc->GetOrigTextureDesc()->GetImagePath(), "Plugins/TestTexturePlugins/NonExistingTexturePath.jpg" );
+    EXPECT_NE( texDesc->GetOrigTextureDesc()->GetImagePath(), imagePath_32x32 );
+
+    auto fallbackDesc = DefaultAssets::Instance().GetFallbackDesc< TextureAssetDesc >();
+
+    EXPECT_EQ( texDesc->GetOrigTextureDesc()->GetImagePath(), fallbackDesc->GetOrigTextureDesc()->GetImagePath() );
+}
 
 
 //// ***********************
-//// Providing path to non existing file should load fallback texture.
-//SIMPLE_FRAMEWORK_TEST_IN_SUITE( Plugins_TexturePlugins, ResourceLoading_TextureDescriptor_InvalidPath )
+//// 
+//SIMPLE_FRAMEWORK_TEST_IN_SUITE( Plugins_TexturePlugins, ResourceLoading_ )
 //{
 //    auto texturePlugin = TestScenesCreator::TexturedRectangle( GetProjectEditor(), "Scene", 400, 400 );
 //
 //    auto texDesc = TextureAssetDesc::Create( "Plugins/TestTexturePlugins/NonExistingTexturePath.jpg", false );
 //
-//
 //}
-
-
-
-
 
