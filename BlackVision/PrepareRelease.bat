6800a2283d10b92658a91a93d910e3dadae3dd7c
Witek <nieznany.sprawiciel@gmail.com>
2018-02-19 14:47:22 +0100

    Abnother paths fix

--- BlackVision/PrepareRelease.bat
+++ BlackVision/PrepareRelease.bat
@@ -4,12 +4,4 @@ setlocal enabledelayedexpansion
 set buildsDir=%1
 set outputDir=%2
 
-set scriptDirectory=%~dp0
-
-
-cd %scriptDirectory%
-
-set buildsInputDir=%buildsDir%
-set buildsOutputDir=%outputDir%
-
-call python PrepareRelease.py %buildsInputDir% %buildsOutputDir%
+call python PrepareRelease.py %buildsDir% %outputDir%
