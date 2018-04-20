8d8a03261b9045a6925aacc2609c884f40fade4c
Witek <nieznany.sprawiciel@gmail.com>
2018-04-20 12:41:54 +0200

    SceneEvent - documentation first part

--- BlackVision/LibBlackVision/Source/Engine/Events/API/SceneEvent.h
+++ BlackVision/LibBlackVision/Source/Engine/Events/API/SceneEvent.h
@@ -16,12 +16,12 @@ namespace bv
 @section SceneEvent_Commands Commands
 
 This event supports following commands:
-- AddScene
-- RemoveScene
-- RemoveAllScenes
-- SetSceneVisible
-- SetSceneInvisible
-- RenameScene
+- @ref SceneEvent_Commands_AddScene "AddScene"
+- @ref SceneEvent_Commands_RemoveScene "RemoveScene"
+- @ref SceneEvent_Commands_RemoveAllScenes "RemoveAllScenes"
+- @ref SceneEvent_Commands_SceneVisiblitity "SetSceneVisible"
+- @ref SceneEvent_Commands_SceneVisiblitity "SetSceneInvisible"
+- @ref SceneEvent_Commands_RenameScene "RenameScene"
 - AttachScene
 - DetachScene
 - MoveScene
@@ -30,7 +30,77 @@ This event supports following commands:
 
 @subsection SceneEvent_Commands_AddScene Adding Scene
 
+Creates and adds new scene on the end of scenes list.
 
+@code{.json}
+{
+    "Event" : "SceneEvent",
+    "EventID" : "3024",
+    "Command" : "AddScene",
+    "NewSceneName" : "Shapes2D_Examples.scn"
+}
+@endcode
+
+@subsection SceneEvent_Commands_RemoveScene Removing Scene
+
+Unloads scene from engine.
+
+@code{.json}
+{
+    "Event" : "SceneEvent",
+    "EventID" : "3040",
+    "Command" : "RemoveScene",
+    "SceneName" : "Shapes2D_Examples.scn"
+}
+@endcode
+
+@subsection SceneEvent_Commands_RemoveAllScenes Removing All Scenes
+
+Unloads all loaded @ref Scenes "Scenes".
+@code{.json}
+{
+    "Event" : "SceneEvent",
+    "EventID" : "3042",
+    "Command" : "RemoveAllScenes"
+}
+@endcode
+
+@subsection SceneEvent_Commands_SceneVisiblitity Scene visibility
+
+Send this event to make Scene visible.
+@code{.json}
+{
+    "Event" : "SceneEvent",
+    "EventID" : "3047",
+    "Command" : "SetSceneVisible",
+    "SceneName" : "Shapes2D_Examples.scn"
+}
+@endcode
+
+To hide scene:
+
+@code{.json}
+{
+    "Event" : "SceneEvent",
+    "EventID" : "3051",
+    "Command" : "SetSceneInvisible",
+    "SceneName" : "Shapes2D_Examples.scn"
+}
+@endcode
+
+@subsection SceneEvent_Commands_RenameScene Renaming Scene
+
+Unloads scene from engine.
+
+@code{.json}
+{
+    "Event" : "SceneEvent",
+    "EventID" : "3062",
+    "Command" : "RenameScene",
+    "SceneName" : "Shapes2D_Examples.scn",
+    "NewSceneName" : "Shapes.scn"
+}
+@endcode
 
 */
 class SceneEvent : public RemoteEvent
